89f80b37beccd4deef075cf90f7ea22b54ee1818
renewiegandt <rene.wiegandt@mpi-bn.mpg.de>
2022-06-29 14:34:33 +0200

    Fix import

--- tests/test_info_txt.py
+++ tests/test_info_txt.py
@@ -1,8 +1,14 @@
+import pytest
+import sctoolbox.checker as checker
 
-def test_fetch_info_path():
-    assert()
+def test_fetch_info_path(tmpdir):
+    # Todo mock
+    file = tmpdir.join("info.txt")
+    checker.write_info_txt("/test/path/", file.strpath)
+    result_path = checker.fetch_info_txt(file.strpath)
+    assert result_path == '/test/path/'
 
 def test_write_info_txt(tmpdir):
     file = tmpdir.join("info.txt")
-    write_info_txt("/test/path/", file.strpath)
+    checker.write_info_txt("/test/path/", file.strpath)
     assert file.read() == '/test/path/'
