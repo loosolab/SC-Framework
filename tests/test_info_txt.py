3c2c99ddfbbeeb39526373853327a231fae0b68f
renewiegandt <rene.wiegandt@mpi-bn.mpg.de>
2022-06-29 14:53:51 +0200

    Upgrade test_fetch_info_path to use mock

--- tests/test_info_txt.py
+++ tests/test_info_txt.py
@@ -1,11 +1,11 @@
-import pytest
+from unittest import mock
+import builtins
 import sctoolbox.checker as checker
 
+
+@mock.patch.object(builtins, "open", new_callable=mock.mock_open, read_data="/test/path/")
 def test_fetch_info_path(tmpdir):
-    # Todo mock
-    file = tmpdir.join("info.txt")
-    checker.write_info_txt("/test/path/", file.strpath)
-    result_path = checker.fetch_info_txt(file.strpath)
+    result_path = checker.fetch_info_txt("test")
     assert result_path == '/test/path/'
 
 def test_write_info_txt(tmpdir):
