1d713da732c371d7f7d39798d892d41d13fa08ea
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2022-07-20 17:20:22 +0200

    mask whitespace in filenames

--- sctoolbox/utilities.py
+++ sctoolbox/utilities.py
@@ -355,7 +355,7 @@ def split_bam_clusters(adata,
         output_files = []
         for cluster in clusters:
             # replace special characters in filename with "_" https://stackoverflow.com/a/27647173
-            save_cluster_name = re.sub(r'[\\/*?:"<>|]', '_', cluster)
+            save_cluster_name = re.sub(r'[\\/*?:"<>| ]', '_', cluster)
             out_paths[cluster] = f"{output_prefix}{save_cluster_name}.bam"
             output_files.append(f"{output_prefix}{save_cluster_name}.bam")
 
@@ -414,7 +414,7 @@ def split_bam_clusters(adata,
         output_files = []
         for cluster in clusters:
             # replace special characters in filename with "_" https://stackoverflow.com/a/27647173
-            save_cluster_name = re.sub(r'[\\/*?:"<>|]', '_', cluster)
+            save_cluster_name = re.sub(r'[\\/*?:"<>| ]', '_', cluster)
             f_out = f"{output_prefix}{save_cluster_name}.bam"
             handles[cluster] = pysam.AlignmentFile(f_out, "wb", template=template, threads=pysam_threads)
 
