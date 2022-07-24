e7a5c3bca08b54647f2971a5165b178e9a005fdd
Yousef <ymalayoubi@gmail.com>
2022-07-24 17:50:27 +0200

    fixed some typos

--- tests/test_celltype_annotation.py
+++ tests/test_celltype_annotation.py
@@ -10,26 +10,25 @@ def test_adata():
     adata = ad.read_h5ad(adata_dir+'/adata_scsa.h5ad')
     return adata
 
-def test_adata_uns():
+def fetch_adata_uns(test_adata):
     # fetches adata.uns['rank_genes_groups] as dict from a test adata to use in
     # test_get_rank_genes
-    adata = test_adata()
-    d = adata.uns['rank_genes_groups']
+    d = test_adata.uns['rank_genes_groups']
     return d
 
 
-@pytest.mark.paramertrize("test_species",["Mouse","Human"])
+@pytest.mark.parametrize("test_species",["Mouse","Human"])
 def test_read_scsa_database(test_species):
-    gene_names, gene_ids = celltype_annotation.check_genes_databses()
+    gene_names, gene_ids = celltype_annotation.read_scsa_database(test_species)
 
     assert isinstance(gene_names, list) and isinstance(gene_ids, list)
 
-def test_get_rank_genes():
-    d = test_adata_uns()
+def test_get_rank_genes(test_adata):
+    d = fetch_adata_uns(test_adata)
     genes = celltype_annotation.get_rank_genes(d)
-    assert genes == set(genes)
+    assert len(genes) == len(set(genes))
 
-@pytest.mark.paramertrize("column",["SCSA_pred_celltype","test_1","test_2"])
+@pytest.mark.parametrize("column",["SCSA_pred_celltype","test_1","test_2"])
 def test_run_scsa(test_adata,column):
     adata = celltype_annotation.run_scsa(test_adata,species='Mouse',inplace=False,column_added=column)
     assert column in adata.obs.columns
