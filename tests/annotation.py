bbb5dc8dea059d98c4179fb03a885119a579608b
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2022-07-25 14:54:29 +0200

    Added test for uropa annotation function

---
+++ tests/annotation.py
@@ -0,0 +1,14 @@
+import os
+import scanpy as sc
+import sctoolbox.annotation
+
+
+def test_annotate_features():
+
+    gtf_path = os.path.join(os.path.dirname(__file__), 'data', 'atac', 'mm10_genes.gtf')
+    adata_path = os.path.join(os.path.dirname(__file__), 'data', 'atac', 'mm10_atac.h5ad')
+    adata = sc.read_h5ad(adata_path)
+
+    sctoolbox.annotation.annotate_features(adata, gtf=gtf_path)
+
+    assert 'gene_id' in adata.var.columns
