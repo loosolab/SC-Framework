d482caa3ceecbaa54b7f53eb9c4c65e613382829
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2022-07-26 12:45:40 +0200

    add plot_pca_variance wrong method test

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,3 +1,4 @@
+import pytest
 import sctoolbox.plotting
 import scanpy as sc
 import os
@@ -11,7 +12,16 @@ def adata():
 
 
 def test_plot_pca_variance(adata):
+    """ Test if Axes object is returned. """
     ax = sctoolbox.plotting.plot_pca_variance(adata)
     ax_type = type(ax).__name__
 
     assert ax_type == "AxesSubplot"
+
+def test_plot_pca_variance_fail(adata):
+    """ Test if failes on invalid method. """
+    # generate invalid method
+    invalid = adata.uns.keys().join() + "-invalid"
+
+    with pytest.raises(KeyError):
+        sctoolbox.plotting.plot_pca_variance(adata, method=invalid)
