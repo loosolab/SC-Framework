4a9fb89da70f3a68a1c87398580170f7106ba046
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2022-07-26 13:25:56 +0200

    added adata_overview tests

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -2,13 +2,26 @@ import pytest
 import sctoolbox.plotting
 import scanpy as sc
 import os
+import tempfile
+import shutil
 
-@pytset.fixture
+@pytest.fixture
 def adata():
     """ Load and returns an anndata object. """
     f = os.path.join(os.path.dirname(__file__), 'data', "adata.h5ad")
 
     return sc.read_h5ad(f)
+
+@pytest.fixture
+def tmp_file():
+    """ Return path for a temporary file. """
+    tmpdir = tempfile.mkdtemp()
+
+    yield os.path.join(tmpdir, "output.pdf")
+
+    # clean up directory and contents
+    shutil.rmtree(tmpdir)
+
 
 
 def test_plot_pca_variance(adata):
@@ -25,3 +38,83 @@ def test_plot_pca_variance_fail(adata):
 
     with pytest.raises(KeyError):
         sctoolbox.plotting.plot_pca_variance(adata, method=invalid)
+
+def test_anndata_overview(adata, tmp_file):
+    """ Test anndata_overview success and file generation. """
+    adatas = {"raw": adata}
+
+    assert not os.path.exists(tmp_file)
+
+    sctoolbox.plotting.anndata_overview(
+        adatas=adatas,
+        color_by=adata.obs.columns,
+        plots=["PCA", "PCA-var", "UMAP"],
+        figsize=None,
+        output=None,
+        dpi=300
+    )
+
+    assert not os.path.exists(tmp_file)
+
+    sctoolbox.plotting.anndata_overview(
+        adatas=adatas,
+        color_by=adata.obs.columns,
+        plots=["PCA", "PCA-var", "UMAP"],
+        figsize=None,
+        output=tmp_file,
+        dpi=300
+    )
+
+    assert os.path.exists(tmp_file)
+
+
+def test_anndata_overview_fail_color_by(adata):
+    """ Test invalid parameter inputs """
+    adatas = {"raw": adata}
+
+    # invalid color_by
+    with pytest.raises(ValueError):
+        # no input
+        sctoolbox.plotting.anndata_overview(
+            adatas=adatas,
+            color_by=None,
+            plots=["PCA", "PCA-var", "UMAP"],
+            figsize=None,
+            output=None,
+            dpi=300
+        )
+        # wrong input
+        sctoolbox.plotting.anndata_overview(
+            adatas=adatas,
+            color_by=adata.obs.columns.join() + "-invalid",
+            plots=["PCA", "PCA-var", "UMAP"],
+            figsize=None,
+            output=None,
+            dpi=300
+        )
+
+
+def test_anndata_overview_fail_plots(adata):
+    """ Test invalid parameter inputs """
+    adatas = {"raw": adata}
+
+    # invalid plots
+    with pytest.raises(ValueError):
+        # no input
+        sctoolbox.plotting.anndata_overview(
+            adatas=adatas,
+            color_by=adata.obs.columns,
+            plots=None,
+            figsize=None,
+            output=None,
+            dpi=300
+        )
+        # wrong input
+        sctoolbox.plotting.anndata_overview(
+            adatas=adatas,
+            color_by=adata.obs.columns,
+            plots=["PCA", "invalid"],
+            figsize=None,
+            output=None,
+            dpi=300
+        )
