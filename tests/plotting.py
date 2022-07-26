4cb4275070efb7a5105b2a29a5c8bd344194db78
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2022-07-26 12:26:09 +0200

    use fixture for loading data

--- tests/plotting.py
+++ tests/plotting.py
@@ -2,12 +2,15 @@ import sctoolbox.plotting
 import scanpy as sc
 import os
 
-
-def test_plot_pca_variance():
-
+@pytset.fixture
+def adata():
+    """ Load and returns an anndata object. """
     f = os.path.join(os.path.dirname(__file__), 'data', "adata.h5ad")
-    adata = sc.read_h5ad(f)
 
+    return sc.read_h5ad(f)
+
+
+def test_plot_pca_variance(adata):
     ax = sctoolbox.plotting.plot_pca_variance(adata)
     ax_type = type(ax).__name__
 
