d8d5d898b3c49f90381e7d33e410541d5bd5b531
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2022-08-02 11:33:28 +0200

    refactor docu

--- sctoolbox/annotation.py
+++ sctoolbox/annotation.py
@@ -4,16 +4,15 @@ import sctoolbox.creators as cr
 
 
 def add_cellxgene_annotation(adata, csv):
-    """ Add columns from cellxgene annotation to the adata .obs table.
+    """
+    Add columns from cellxgene annotation to the adata .obs table.
+
     Parameters
-    ------------
-    adata : anndata object
-        The adata object to add annotations to.
+    ----------
+    adata : anndata.AnnData
+        Adata object to add annotations to.
     csv : str
         Path to the annotation file from cellxgene containing cell annotation.
-    Returns
-    --------
-    None - the annotation is added to adata in place.
     """
     anno_table = pd.read_csv(csv, sep=",", comment='#')
     anno_table.set_index("index", inplace=True)
