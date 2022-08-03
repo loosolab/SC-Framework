a9f43c783bbec8eb10e556c3cc7ed7d640c9bb28
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2022-08-03 15:14:27 +0200

    implemented define_PC tests

--- tests/test_define_PC.py
+++ tests/test_define_PC.py
@@ -1,28 +1,35 @@
 import pytest
-import anndata
 import pandas as pd
 import numpy as np
+import scanpy as sc
+import os
 import sctoolbox.analyser as an
 
+
 @pytest.fixture
-def test_adata_vr():
-    adata = anndata.AnnData(pd.DataFrame({"a": pd.Series(1, index=list(range(2)),dtype="float32"),
-                                          "b": pd.Series(1, index=list(range(2)), dtype="float32")},
-                                          index=["a","b","c","d"]))
+def adata():
+    """ Load and returns an anndata object. """
+    f = os.path.join(os.path.dirname(__file__), 'data', "adata.h5ad")
 
-    variance_ratio = np.array([0.08749175, 0.03792242, 0.02643393, 0.02533128, 0.0180286 ,
-                               0.01313281, 0.01159428, 0.01045423, 0.0083562 , 0.00675285,
-                               0.00628902, 0.00555756, 0.00518746, 0.00414544, 0.00378045,
-                               0.00332042, 0.00329669, 0.00297686, 0.00284822, 0.00258963,
-                               0.00246014, 0.00222408, 0.00212752, 0.0019508 , 0.00190878,
-                               0.00179127, 0.00175954, 0.00161796, 0.00157837, 0.00154598,
-                               0.00149027, 0.00147236, 0.00141266, 0.00132336, 0.00126029,
-                               0.00122388, 0.0011817 , 0.0011381 , 0.00108894, 0.00107301,
-                               0.00103355, 0.00102861, 0.00098351, 0.00093241, 0.00090659,
-                               0.0008869 , 0.00086826, 0.00086241, 0.00082759, 0.000824])
+    return sc.read_h5ad(f)
 
-    adata.uns["pca"] = {"variance_ratio": variance_ratio}
-    return adata
 
-def test_define_PC(test_adata_vr):
-    print(test_adata_vr)
+@pytest.fixture
+def adata_no_pca():
+    """ Adata without PCA. """
+    anndata = adata()
+
+    # remove pca
+    anndata.uns.pop("pca")
+
+    return anndata
+
+
+def test_define_PC(adata):
+    """ Test if threshold is returned. """
+    assert isinstance(an.define_PC(adata), int)
+
+def test_define_PC_error(adata_no_pca):
+    """ Test if error without PCA. """
+    with pytest.raises(ValueError, match="PCA not found! Please make sure to compute PCA before running this function."):
+        an.define_PC(adata_no_pca)
