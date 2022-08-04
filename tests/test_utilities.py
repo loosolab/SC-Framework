bda6bc68fe31cb82a432f326720082767584d847
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2022-08-04 20:15:24 +0200

    Add additional tests for utilities

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 import sctoolbox.utilities as utils
 
 @pytest.fixture
@@ -6,10 +7,41 @@ def berries():
     return ["blueberry", "strawberry", "blackberry"]
 
 
-def test_longest_common_suffix(berries):
+def test_clean_flanking_strings():
 
+    paths = ["path_a.txt", "path_b.txt", "path_c.txt"]
+    cleaned = utils.clean_flanking_strings(paths)
+
+    assert cleaned == ["a", "b", "c"]
+
+
+def test_longest_common_suffix(berries):
     suffix = utils.longest_common_suffix(berries)
     assert suffix == "berry"
+
+
+def test_create_dir():
+    utils.create_dir("testdir")
+    assert os.path.isdir("testdir")
+
+
+def test_is_notebook():
+    boolean = utils._is_notebook()
+    assert boolean is False  # testing environment is not notebook
+
+
+def test_is_str_numeric():
+
+    expect_true = utils.is_str_numeric("1.3")
+    expect_false = utils.is_str_numeric("astring")
+
+    assert expect_true
+    assert expect_false is False
+
+
+def test_check_module():
+    with pytest.raises(Exception):
+        utils.check_module("nonexisting_module")
 
 
 def test_remove_prefix():
@@ -29,3 +61,18 @@ def test_split_list(berries):
 
     split = utils.split_list(berries, 2)
     assert split == [["blueberry", "blackberry"], ["strawberry"]]
+
+def test_read_list_file(berries):
+
+    path = "berries.txt"
+    utils.write_list_file(berries, path)
+    berries_read = utils.read_list_file(path)
+
+    assert berries == berries_read
+
+def test_write_list_file(berries):
+
+    path = "berries.txt"
+    utils.write_list_file(berries, path)
+
+    assert os.path.isfile(path)
