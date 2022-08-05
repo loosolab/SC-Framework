7e7bc8663aa1283dafd642c9855a9c96361bbf4b
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2022-08-05 13:21:42 +0200

    Added test data for functions in annotation.py

---
+++ tests/test_annotation.py
@@ -0,0 +1,23 @@
+from cgi import test
+import sctoolbox.annotation as anno
+import scanpy as sc
+import os
+
+@pytest.fixture
+def adata():
+    adata_f = os.path.join(os.path.dirname(__file__), 'data', 'adata.h5ad')
+    return sc.read_h5ad(adata_f)
+
+def test_add_cellxgene_annotation(adata):
+    
+    csv_f = os.path.join(os.path.dirname(__file__), 'data', 'cellxgene_anno.csv')
+    anno.add_cellxgene_annotation(adata, csv_f)
+
+    assert "cellxgene_clusters" in adata.obs.columns
+
+def test_annot_HVG(adata):
+
+    sc.pp.log1p(adata)
+    anno.annot_HVG(adata)
+
+    assert "highly_variable" in adata.var.columns
