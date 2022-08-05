9446de0d2be92433d89eeedc5efcc6d7c8e7898c
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2022-08-05 14:20:10 +0200

    Added function to check if an array contains only integers

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,9 +1,19 @@
 import pytest
+import numpy as np
 import sctoolbox.utilities as utils
 
 @pytest.fixture
 def berries():
     return ["blueberry", "strawberry", "blackberry"]
+
+
+arr_ints = np.random.randint(10, size=(10,10))
+arr_ints2 = arr_ints.astype(float)
+arr_floats = np.random.rand(10, 10)
+
+@pytest.mark.parametrize("arr, boolean", [(arr_ints, True), (arr_ints2, True), (arr_floats, False)])
+def test_is_integer_array(arr, boolean):
+    assert utils.is_integer_array(arr) is boolean
 
 
 def test_longest_common_suffix(berries):
