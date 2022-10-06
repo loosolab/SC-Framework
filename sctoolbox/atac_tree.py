be18e599d275b04e8f02fb061fb1ce1d3599a11c
Jan Detleffsen <jan.detleffsen@andra.de>
2022-10-06 11:15:40 +0200

    Added assembled_anndata to atac_tree.py

--- sctoolbox/atac_tree.py
+++ sctoolbox/atac_tree.py
@@ -7,10 +7,14 @@ class ATAC_tree(generalized_tree.Tree):
     This extends Tree by ATAC related directories
     '''
 
+    # directories
     _pre_pro_dir = None
+    _assembled_anndata_dir = None
     _qc_plots_dir = None
 
-    # ATAC related notebooks outputs
+    # files
+    _assembled_anndata = None
+
     def setupDir(self):
         '''
         Extends the setupDir() function of the super-class Tree from generalized_tree.py.
@@ -22,7 +26,16 @@ class ATAC_tree(generalized_tree.Tree):
         # call super function
         super(ATAC_tree, self).setupDir()
 
-        # setup ATAC related directories
+        ################################################################################################################
+        # setup ATAC related directories:
+
+        # Assembling notebook related paths
+        # anndata files directory
+        self._assembled_anndata_dir = os.path.join(self._assemble_dir, 'anndata')
+        # anndata file of the run
+        self._assembled_anndata = os.path.join(self._assembled_anndata_dir, run + '.h5ad')
+
+        # QC notebook related paths
         self._qc_plots_dir = os.path.join(self.qc_dir, 'plots')
         # ADD ATAC PATHS
         ################################
@@ -41,6 +54,22 @@ class ATAC_tree(generalized_tree.Tree):
     @pre_pro_dir.setter
     def pre_pro_dir(self, value):
         self._pre_pro_dir = value
+
+    @property
+    def assembled_anndata_dir(self):
+        return self._assembled_anndata_dir
+
+    @assembled_anndata_dir.setter
+    def assembled_anndata_dir(self, value):
+        self._assembled_anndata_dir = value
+
+    @property
+    def assembled_anndata(self):
+        return self._assembled_anndata
+
+    @assembled_anndata.setter
+    def assembled_anndata(self, value):
+        self._assembled_anndata = value
 
     @property
     def qc_plots_dir(self):
