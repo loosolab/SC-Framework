60541141a199ae97614ab6fd23367d382fd3193a
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2022-10-30 21:25:46 +0100

    Progress bar to monitor jobs from multiprocessing

--- sctoolbox/utilities.py
+++ sctoolbox/utilities.py
@@ -9,6 +9,8 @@ import sctoolbox.checker as ch
 import sctoolbox.creators as cr
 import matplotlib.pyplot as plt
 import matplotlib
+import time
+
 from os.path import join, dirname, exists
 from pathlib import Path
 from IPython.core.magic import register_line_magic
@@ -43,6 +45,62 @@ def get_package_versions():
             print(f"Error reading version for package: {s}")
 
     return package_dict
+
+
+def get_pbar(total, description):
+    """
+    Get a progress bar depending on whether the user is using a notebook or not.
+
+    Parameters
+    ----------
+    total : int
+        Total number elements to be shown in the progress bar.
+    description : str
+        Description to be shown in the progress bar.
+
+    Returns
+    -------
+    tqdm
+        A progress bar object.
+    """
+
+    if _is_notebook() is True:
+        from tqdm import tqdm_notebook as tqdm
+    else:
+        from tqdm import tqdm
+
+    pbar = tqdm(total=total, desc=description)
+    return pbar
+
+
+def monitor_jobs(jobs, description="Progress"):
+    """
+    Monitor the status of jobs submitted to a pool.
+
+    Parameters
+    ----------
+    jobs : list of job objects
+        List of job objects, e.g. as returned by pool.map_async().
+    description : str, default "Progress"
+        Description to be shown in the progress bar.
+    """
+
+    if isinstance(jobs, dict):
+        jobs = list(jobs.values())
+
+    # Wait for all jobs to finish
+    n_ready = sum([job.ready() for job in jobs])
+    pbar = get_pbar(len(jobs), description)
+    while n_ready != len(jobs):
+        if n_ready != pbar.n:
+            pbar.n = n_ready
+            pbar.refresh()
+        time.sleep(1)
+        n_ready = sum([job.ready() for job in jobs])
+
+    pbar.n = n_ready  # update progress bar to 100%
+    pbar.refresh()
+    pbar.close()
 
 
 # ------------------ Type checking ------------------ #
