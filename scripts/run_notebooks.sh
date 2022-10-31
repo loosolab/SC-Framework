ee30176ae35143bef2bf1012a821e313f1c0de11
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2022-10-31 11:30:41 +0100

    Added test for notebooks for pushing to main

---
+++ scripts/run_notebooks.sh
@@ -0,0 +1,4 @@
+jupyter nbconvert --to notebook --execute notebooks/1_assembling_anndata.ipynb
+jupyter nbconvert --to notebook --execute notebooks/2_QC_filtering.ipynb
+jupyter nbconvert --to notebook --execute notebooks/3_batch.ipynb
+jupyter nbconvert --to notebook --execute notebooks/4_clustering.ipynb
