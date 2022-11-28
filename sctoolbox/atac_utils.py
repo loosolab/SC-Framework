0a059c3edefce6336fd05010345109eaebf17edf
Jan Detleffsen <jan.detleffsen@andra.de>
2022-11-29 00:51:24 +0100

    added plotting

--- sctoolbox/atac_utils.py
+++ sctoolbox/atac_utils.py
@@ -172,19 +172,19 @@ def plot_ov_hist(adata, threshold_features=1000):
     :return:
     """
     # show open features per cell
-    min_features = threshold_features
+    #min_features = threshold_features
 
-    epi.pp.coverage_cells(adata, binary=True, log=False, bins=50,
-                          threshold=min_features)
-    # epi.pp.coverage_cells(adata, binary=True, log=10, bins=50, threshold=min_features)
+    epi.pp.coverage_cells(adata, binary=True, log=False, bins=50)
+    epi.pp.coverage_cells(adata, binary=True, log=10, bins=50)
 
     # show numbers of cells sharing features
 
-    epi.pp.coverage_features(adata, binary=True, log=False, bins=50,
-                             threshold=None)
-    # epi.pp.coverage_features(adata, binary=True, log=10, bins=50, threshold=None)
+    epi.pp.coverage_features(adata, binary=True, log=False, bins=50)
+    epi.pp.coverage_features(adata, binary=True, log=10, bins=50)
 
     epi.pp.cal_var(adata)
+
+    return adata
 
 
 def plot_obs_violin(adata, obs_cols):
@@ -229,88 +229,127 @@ def barcode_index(adata):
         print('barcodes are already the index')
 
 
+def violin_HVF_distribution(adata):
+    """
+    plot the distribution of the HVF
+    :param adata:
+    :return:
+    """
+    # get the number of cells per highly variable feature
+    hvf_var = adata.var[adata.var['highly_variable'] == True]
+    n_cells = hvf_var['n_cells_by_counts']
+    n_cells.reset_index(drop=True, inplace=True)
+    # violin plot
+    fig, ax = plt.subplots()
+    ax.violinplot(n_cells, showmeans=True, showmedians=True)
+    ax.set_title('Distribution of the number of cells per highly variable feature')
+    ax.set_ylabel('Number of cells')
+    ax.set_xlabel('Highly variable features')
+    plt.show()
+
+
+def scatter_HVF_distribution(adata):
+    """
+    plot the distribution of the HVF
+    :param adata:
+    :return:
+    """
+    variabilities = adata.var[['variability_score', 'n_cells']]
+    fig, ax = plt.subplots()
+    ax.scatter(variabilities['n_cells'], variabilities['variability_score'])
+    ax.set_title('Distribution of the number of cells and variability score per feature')
+    ax.set_xlabel('Number of cells')
+    ax.set_ylabel('variability score')
+    plt.show()
+
+
 if __name__ == '__main__':
 
-    qc_columns = {}
-    qc_columns['n_features_by_counts'] = None
-    qc_columns['log1p_n_features_by_counts'] = None
-    qc_columns['total_counts'] = None
-    qc_columns['log1p_total_counts'] = None
-    qc_columns['mean_insertsize'] = None
-    qc_columns['n_total_fragments'] = None
-    qc_columns['n_fragments_in_promoters'] = None
-    qc_columns['pct_fragments_in_promoters'] = None
-    qc_columns['blacklist_overlaps'] = None
-    qc_columns['TN'] = 'TN'
-    qc_columns['UM'] = 'UM'
-    qc_columns['PP'] = 'PP'
-    qc_columns['UQ'] = 'UQ'
-    qc_columns['CM'] = 'CM'
 
+    adata = epi.read_h5ad('/home/jan/python-workspace/sc-atac/processed_data/Esophagus/norm_correction/anndata/Esophagus.h5ad')
+    violin_HVF_distribution(adata)
+    scatter_HVF_distribution(adata)
 
-    adata = assemble_from_h5ad(['/mnt/workspace/jdetlef/data/anndata/cropped_146.h5ad'], qc_columns, column='sample', conditions=None)
-    #adata = epi.read_h5ad('/mnt/workspace/jdetlef/processed_data/Esophagus/assembling/anndata/Esophagus.h5ad')
-
-
-    # Filter to use:
-    n_features_filter = True  # True or False; filtering out cells with numbers of features not in the range defined below
-    mean_insertsize_filter = True  # True or False; filtering out cells with mean insertsize not in the range defined below
-    filter_pct_fp = True  # True or False; filtering out cells with promotor_enrichment not in the range defined below
-    filter_n_fragments = True  # True or False; filtering out cells with promotor_enrichment not in the range defined below
-    filter_chrM_fragments = True  # True or False; filtering out cells with promotor_enrichment not in the range defined below
-    filter_uniquely_mapped_fragments = True  # True or False; filtering out cells with promotor_enrichment not in the range defined
-
-    # if this is True thresholds below are ignored
-    only_automatic_thresholds = True  # True or False; to use automatic thresholds
-
-    ############################# set default values #######################################
+    # qc_columns = {}
+    # qc_columns['n_features_by_counts'] = None
+    # qc_columns['log1p_n_features_by_counts'] = None
+    # qc_columns['total_counts'] = None
+    # qc_columns['log1p_total_counts'] = None
+    # qc_columns['mean_insertsize'] = None
+    # qc_columns['n_total_fragments'] = None
+    # qc_columns['n_fragments_in_promoters'] = None
+    # qc_columns['pct_fragments_in_promoters'] = None
+    # qc_columns['blacklist_overlaps'] = None
+    # qc_columns['TN'] = 'TN'
+    # qc_columns['UM'] = 'UM'
+    # qc_columns['PP'] = 'PP'
+    # qc_columns['UQ'] = 'UQ'
+    # qc_columns['CM'] = 'CM'
     #
-    # This will be applied to all samples the thresholds can be changed manually when plotted
-    # if thresholds None they are set automatically
-
-    # default values n_features
-    min_features = None
-    max_features = None
-
-    # default mean_insertsize
-    upper_threshold_mis = None
-    lower_threshold_mis = None
-
-    # default promotor enrichment
-    upper_threshold_pct_fp = 0.4
-    lower_threshold_pct_fp = 0.1
-
-    # default number of fragments
-    upper_thr_fragments = 200000
-    lower_thr_fragments = 0
-
-    # default number of fragments in chrM
-    upper_thr_chrM_fragments = 10000
-    lower_thr_chrM_fragments = 0
-
-    # default number of uniquely mapped fragments
-    upper_thr_um = 20000
-    lower_thr_um = 0
-    manual_thresholds = {}
-    if n_features_filter:
-        manual_thresholds['n_features_by_counts'] = {'min': min_features, 'max': max_features}
-
-    if mean_insertsize_filter:
-        manual_thresholds['mean_insertsize'] = {'min': lower_threshold_mis, 'max': upper_threshold_mis}
-
-    if filter_pct_fp:
-        manual_thresholds['pct_fragments_in_promoters'] = {'min': lower_threshold_pct_fp, 'max': upper_threshold_pct_fp}
-
-    if filter_n_fragments:
-        manual_thresholds['TN'] = {'min': lower_thr_fragments, 'max': upper_thr_fragments}
-
-    if filter_chrM_fragments:
-        manual_thresholds['CM'] = {'min': lower_thr_chrM_fragments, 'max': upper_thr_chrM_fragments}
-
-    if filter_uniquely_mapped_fragments:
-        manual_thresholds['UM'] = {'min': lower_thr_um, 'max': upper_thr_um}
-
-    #adata.obs = adata.obs.fillna(0)
-    thresholds = get_thresholds_atac_wrapper(adata, manual_thresholds, only_automatic_thresholds)
-
-    print(thresholds)
+    #
+    # adata = assemble_from_h5ad(['/mnt/workspace/jdetlef/data/anndata/cropped_146.h5ad'], qc_columns, column='sample', conditions=None)
+    # #adata = epi.read_h5ad('/mnt/workspace/jdetlef/processed_data/Esophagus/assembling/anndata/Esophagus.h5ad')
+    #
+    #
+    # # Filter to use:
+    # n_features_filter = True  # True or False; filtering out cells with numbers of features not in the range defined below
+    # mean_insertsize_filter = True  # True or False; filtering out cells with mean insertsize not in the range defined below
+    # filter_pct_fp = True  # True or False; filtering out cells with promotor_enrichment not in the range defined below
+    # filter_n_fragments = True  # True or False; filtering out cells with promotor_enrichment not in the range defined below
+    # filter_chrM_fragments = True  # True or False; filtering out cells with promotor_enrichment not in the range defined below
+    # filter_uniquely_mapped_fragments = True  # True or False; filtering out cells with promotor_enrichment not in the range defined
+    #
+    # # if this is True thresholds below are ignored
+    # only_automatic_thresholds = True  # True or False; to use automatic thresholds
+    #
+    # ############################# set default values #######################################
+    # #
+    # # This will be applied to all samples the thresholds can be changed manually when plotted
+    # # if thresholds None they are set automatically
+    #
+    # # default values n_features
+    # min_features = None
+    # max_features = None
+    #
+    # # default mean_insertsize
+    # upper_threshold_mis = None
+    # lower_threshold_mis = None
+    #
+    # # default promotor enrichment
+    # upper_threshold_pct_fp = 0.4
+    # lower_threshold_pct_fp = 0.1
+    #
+    # # default number of fragments
+    # upper_thr_fragments = 200000
+    # lower_thr_fragments = 0
+    #
+    # # default number of fragments in chrM
+    # upper_thr_chrM_fragments = 10000
+    # lower_thr_chrM_fragments = 0
+    #
+    # # default number of uniquely mapped fragments
+    # upper_thr_um = 20000
+    # lower_thr_um = 0
+    # manual_thresholds = {}
+    # if n_features_filter:
+    #     manual_thresholds['n_features_by_counts'] = {'min': min_features, 'max': max_features}
+    #
+    # if mean_insertsize_filter:
+    #     manual_thresholds['mean_insertsize'] = {'min': lower_threshold_mis, 'max': upper_threshold_mis}
+    #
+    # if filter_pct_fp:
+    #     manual_thresholds['pct_fragments_in_promoters'] = {'min': lower_threshold_pct_fp, 'max': upper_threshold_pct_fp}
+    #
+    # if filter_n_fragments:
+    #     manual_thresholds['TN'] = {'min': lower_thr_fragments, 'max': upper_thr_fragments}
+    #
+    # if filter_chrM_fragments:
+    #     manual_thresholds['CM'] = {'min': lower_thr_chrM_fragments, 'max': upper_thr_chrM_fragments}
+    #
+    # if filter_uniquely_mapped_fragments:
+    #     manual_thresholds['UM'] = {'min': lower_thr_um, 'max': upper_thr_um}
+    #
+    # #adata.obs = adata.obs.fillna(0)
+    # thresholds = get_thresholds_atac_wrapper(adata, manual_thresholds, only_automatic_thresholds)
+    #
+    # print(thresholds)
