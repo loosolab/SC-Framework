9fe1ad44af21d58e8b09d8169908ad2a979ae162
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2022-11-29 13:25:20 +0100

    You won lint.. i give up

--- sctoolbox/assemblers.py
+++ sctoolbox/assemblers.py
@@ -247,8 +247,7 @@ def from_mtx(path, mtx="*_matrix.mtx*", barcodes="*_barcodes.tsv*", genes="*_gen
     it = iter([mtx, barcodes, genes])
     the_len = len(next(it))
     if not all(len(list_len) == the_len for list_len in it):
-        raise ValueError('Found different quantitys of mtx, genes, barcode files.\n'
-                          + 'Please check given suffixes or filenames')
+        raise ValueError("Found different quantitys of mtx, genes, barcode files.\n Please check given suffixes or filenames")
 
     if not mtx:
         raise ValueError('No files were found with the given directory and suffixes')
