7cf4b3dbd54afcd477173c8ff7157afa52fbe3aa
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2022-12-13 11:50:39 +0100

    Fix for one-plot QC

--- sctoolbox/qc_filter.py
+++ sctoolbox/qc_filter.py
@@ -693,7 +693,7 @@ def quality_violin(adata, columns,
         figsize = (ncols * 4, nrows * 4)  # static plot can be larger
 
     fig, axarr = plt.subplots(nrows, ncols, figsize=figsize)
-    axes_list = axarr.flatten()
+    axes_list = [axarr] if type(axarr).__name__ == "AxesSubplot" else axarr.flatten()
 
     # Remove empty axes
     for ax in axes_list[len(columns):]:
