79f5a7171a964c8a77dff40de70e7a15553ce046
Yousef Alayoubi <yousef.alayoubi@mpi-bn.mpg.de>
2022-12-19 11:02:45 +0100

    add tests for tfidf, lsi and atac_norm

--- tests/test_atac.py
+++ tests/test_atac.py
@@ -2,6 +2,8 @@ import pytest
 import sctoolbox.atac
 import os
 import scanpy as sc
+import numpy as np
+import anndata as ad
 import yaml
 
 
@@ -11,6 +13,32 @@ def adata():
     adata = sc.read_h5ad(os.path.join(os.path.dirname(__file__), 'data', 'atac', 'mm10_atac.h5ad'))
     return adata
 
+
+# adapted from muon package
+@pytest.fixture
+def tfidf_x():
+    np.random.seed(2020)
+    x = np.abs(np.random.normal(size=(4, 5)))
+    adata_X = ad.AnnData(x)
+    return adata_X
+
+# adapted from muon package
+def test_tfidf(tfidf_x):
+    sctoolbox.atac.tfidf(tfidf_x, log_tf=True, log_idf=True)
+    assert str("%.3f" % tfidf_x.X[0, 0]) == "4.659"
+    assert str("%.3f" % tfidf_x.X[3, 0]) == "4.770"
+
+
+def test_lsi(adata):
+    sctoolbox.atac.tfidf(adata)
+    sctoolbox.atac.lsi(adata)
+    assert "X_lsi" in adata.obsm and "lsi" in adata.uns and "LSI" in adata.varm
+    
+
+def test_atac_norm(adata):
+    adata_tfidf, adata_total = sctoolbox.atac.atac_norm(adata, condition_col='Sample')
+    assert "X_lsi" in adata_tfidf.obsm and "lsi" in adata_tfidf.uns and "LSI" in adata_tfidf.varm
+    assert "X_pca" in adata_total.obsm and "pca" in adata_total.uns and "PCs" in adata_total.varm
 
 def test_write_TOBIAS_config():
 
