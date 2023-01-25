b0a020b24790e6fea19f0951f8803a06997ffcb4
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2023-01-25 14:28:05 +0100

    linting

--- sctoolbox/atac_utils.py
+++ sctoolbox/atac_utils.py
@@ -239,7 +239,7 @@ def violin_HVF_distribution(adata):
     :return:
     """
     # get the number of cells per highly variable feature
-    hvf_var = adata.var[adata.var['highly_variable'] == True]
+    hvf_var = adata.var[adata.var['highly_variable']]  # 'highly_variable' is a boolean column
     n_cells = hvf_var['n_cells_by_counts']
     n_cells.reset_index(drop=True, inplace=True)
     # violin plot
