b2f847c61a5193e77d7157b75b946cef898cc7b3
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2023-01-27 18:20:33 +0100

    Added debug to notebook runs

--- scripts/run_notebooks.sh
+++ scripts/run_notebooks.sh
@@ -1,5 +1,5 @@
 cd notebooks
-papermill --log-output -k sctoolbox 1_assembling_anndata.ipynb out.ipynb
-papermill --log-output -k sctoolbox 2_QC_filtering.ipynb out.ipynb
-papermill --log-output -k sctoolbox 3_batch.ipynb out.ipynb
-papermill --log-output -k sctoolbox 4_clustering.ipynb out.ipynb
+papermill --log-output -k sctoolbox --log-level DEBUG 1_assembling_anndata.ipynb out.ipynb
+papermill --log-output -k sctoolbox --log-level DEBUG 2_QC_filtering.ipynb out.ipynb
+papermill --log-output -k sctoolbox --log-level DEBUG 3_batch.ipynb out.ipynb
+papermill --log-output -k sctoolbox --log-level DEBUG 4_clustering.ipynb out.ipynb
