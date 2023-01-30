222415d83e08e473417a33207eb31a2a048ac7dd
Jan Detleffsen <jan.detleffsen@mpi-bn.mpg.de>
2023-01-30 12:43:16 +0100

    refactor annotation atac_utils

--- sctoolbox/atac_utils.py
+++ sctoolbox/atac_utils.py
@@ -3,12 +3,18 @@ import scanpy as sc
 import re
 import sctoolbox.qc_filter as qc_filter
 import sctoolbox.creators as cr
+import sctoolbox.utilities as utils
 import episcanpy as epi
 import anndata as ad
 from matplotlib import pyplot as plt
 
 
-def assemble_from_h5ad(h5ad_files, qc_columns, column='sample', from_snapatac=True, peak_columns=None, conditions=None):
+def assemble_from_h5ad(h5ad_files,
+                       qc_columns,
+                       column='sample',
+                       coordinate_cols=['chr', 'start', 'end'],
+                       set_index=True,
+                       index_from=None):
     '''
     Function to assemble multiple adata files into a single adata object with a sample column in the
     adata.obs table. This concatenates adata.obs and merges adata.uns.
@@ -36,28 +42,38 @@ def assemble_from_h5ad(h5ad_files, qc_columns, column='sample', from_snapatac=Tr
         sample = 'sample' + str(counter)
 
         adata = epi.read_h5ad(h5ad_path)
-        if from_snapatac:
+        if set_index:
+            format_index(adata, index_from)
 
-            # split the peak column into chromosome start and end
-            adata.var[['peak_chr', 'start_end']] = adata.var['name'].str.split(':', expand=True)
-            adata.var[['peak_start', 'peak_end']] = adata.var['start_end'].str.split('-', expand=True)
-            # remove start_end column
-            adata.var.drop('start_end', axis=1, inplace=True)
+        #     # split the peak column into chromosome start and end
+        #     adata.var[['peak_chr', 'start_end']] = adata.var['name'].str.split(':', expand=True)
+        #     adata.var[['peak_start', 'peak_end']] = adata.var['start_end'].str.split('-', expand=True)
+        #     # remove start_end column
+        #     adata.var.drop('start_end', axis=1, inplace=True)
+        #
+        #     # exclude the b' and ' from the chromosome
+        #     adata.var['peak_chr'] = adata.var['peak_chr'].str.replace("b'", "")
+        #     adata.var['peak_chr'] = adata.var['peak_chr'].str.replace("'", "")
+        #
+        #     adata.var = adata.var.set_index('name')
+        #
+        # else:
+        #     if peak_columns is not None:
+        #         # rename adata.var columns to peak_chr, peak_start, peak_end
+        #         adata.var.rename(columns=peak_columns, inplace=True)
+        #
+        #     else:
+        #         # raise warning if peak_columns is not set
+        #         print('peak_columns is not set. Please set peak_columns to rename adata.var columns to peak_chr, peak_start, peak_end')
 
-            # exclude the b' and ' from the chromosome
-            adata.var['peak_chr'] = adata.var['peak_chr'].str.replace("b'", "")
-            adata.var['peak_chr'] = adata.var['peak_chr'].str.replace("'", "")
-
-            adata.var = adata.var.set_index('name')
-
+        # Establish columns for coordinates
+        if coordinate_cols is None:
+            coordinate_cols = adata.var.columns[:3]  # first three columns are coordinates
         else:
-            if peak_columns is not None:
-                # rename adata.var columns to peak_chr, peak_start, peak_end
-                adata.var.rename(columns=peak_columns, inplace=True)
+            utils.check_columns(adata.var, coordinate_cols,
+                                "coordinate_cols")  # Check that coordinate_cols are in adata.var)
 
-            else:
-                # raise warning if peak_columns is not set
-                print('peak_columns is not set. Please set peak_columns to rename adata.var columns to peak_chr, peak_start, peak_end')
+        format_adata_var(adata, coordinate_cols, coordinate_cols)
 
         # Add information to the infoprocess
         cr.build_infor(adata, "Input_for_assembling", h5ad_path)
@@ -88,6 +104,92 @@ def assemble_from_h5ad(h5ad_files, qc_columns, column='sample', from_snapatac=Tr
         adata.var = pd.merge(adata.var, value.var, left_index=True, right_index=True)
 
     return adata
+
+
+def format_index(adata, from_column=None):
+    """
+    This formats the index of adata.var by the pattern ["chr", "start", "stop"]
+    Parameters
+    ----------
+    adata: anndata.AnnData
+    from_column: None or column name (str) in adata.var to be set as index
+
+    Returns
+    -------
+
+    """
+    if from_column == None:
+        entry = adata.var.index[0]
+        index_type = get_index_type(entry)
+
+        if index_type == 'snapatac':
+
+            # split the peak column into chromosome start and end
+            adata.var[['peak_chr', 'start_end']] = adata.var['name'].str.split(':', expand=True)
+            adata.var[['peak_start', 'peak_end']] = adata.var['start_end'].str.split('-', expand=True)
+            # remove start_end column
+            adata.var.drop('start_end', axis=1, inplace=True)
+
+            # exclude the b' and ' from the chromosome
+            adata.var['peak_chr'] = adata.var['peak_chr'].str.replace("b'", "")
+            adata.var['peak_chr'] = adata.var['peak_chr'].str.replace("'", "")
+
+            adata.var = adata.var.set_index('name')
+
+        elif index_type == "start_name":
+            coordinate_pattern = r"(chr[0-9XYM]+)+[\_\:\-]+[0-9]+[\_\:\-]+[0-9]+"
+            new_index = []
+            for line in adata.var.index:
+                new_index.append(re.search(coordinate_pattern, line).group(0))
+            adata.var['new_index'] = new_index
+            adata.var.set_index('new_index', inplace=True)
+
+    else:
+        entry = list(adata.var[from_column])
+        index_type = get_index_type(entry)
+
+        if index_type == 'snapatac':
+
+            # split the peak column into chromosome start and end
+            adata.var[['peak_chr', 'start_end']] = adata.var['name'].str.split(':', expand=True)
+            adata.var[['peak_start', 'peak_end']] = adata.var['start_end'].str.split('-', expand=True)
+            # remove start_end column
+            adata.var.drop('start_end', axis=1, inplace=True)
+
+            # exclude the b' and ' from the chromosome
+            adata.var['peak_chr'] = adata.var['peak_chr'].str.replace("b'", "")
+            adata.var['peak_chr'] = adata.var['peak_chr'].str.replace("'", "")
+
+            adata.var = adata.var.set_index('name')
+
+        elif index_type == "start_name":
+            coordinate_pattern = r"(chr[0-9XYM]+)+[\_\:\-]+[0-9]+[\_\:\-]+[0-9]+"
+            new_index = []
+            for line in adata.var[from_column]:
+                new_index.append(re.search(coordinate_pattern, line).group(0))
+            adata.var['new_index'] = new_index
+            adata.var.set_index('new_index', inplace=True)
+
+def get_index_type(entry):
+    """
+    Check the format of the index by regex
+    Parameters
+    ----------
+    entry
+
+    Returns
+    -------
+
+    """
+
+    regex_snapatac = r"^b'(chr[0-9]+)+'[\_\:\-]+[0-9]+[\_\:\-]+[0-9]+" # matches: b'chr1':12324-56757
+    regex_start_name = r"^.+(chr[0-9]+)+[\_\:\-]+[0-9]+[\_\:\-]+[0-9]+" # matches: some_name-chr1:12343-76899
+
+
+    if re.match(regex_snapatac, entry):
+        return 'snapatac'
+    if re.match(regex_start_name, entry):
+        return 'start_name'
 
 
 def get_keys(adata, manual_thresholds):
@@ -144,7 +246,7 @@ def build_default_thresholds(adata, manual_thresholds, groupby=None):
 def get_thresholds_atac_wrapper(adata, manual_thresholds, only_automatic_thresholds=True, groupby=None):
     """
     return the thresholds for the filtering
-    :param adata:
+    :param adata: anndata.AnnData
     :param manual_thresholds:
     :param automatic_thresholds:
     :return:
@@ -262,6 +364,101 @@ def scatter_HVF_distribution(adata):
     plt.show()
 
 
+def validate_regions(adata, coordinate_columns):
+    """ Checks if the regions in adata.var are valid.
+
+    Parameters
+    -----------
+    adata : AnnData
+        AnnData object containing the regions to be checked.
+    coordinate_columns : list of str
+        List of length 3 for column names in adata.var containing chr, start, end coordinates. """
+
+    # Test whether the first three columns are in the right format
+    chr, start, end = coordinate_columns
+
+    # Test if coordinate columns are in adata.var
+    utils.check_columns(adata.var, coordinate_columns, "adata.var")
+
+    # Test whether the first three columns are in the right format
+    for _, line in adata.var.to_dict(orient="index").items():
+        valid = False
+
+        if isinstance(line[chr], str) and isinstance(line[start], int) and isinstance(line[end], int):
+            if line[start] <= line[end]:  # start must be smaller than end
+                valid = True  # if all tests passed, the line is valid
+
+        if valid is False:
+            raise ValueError("The region {0}:{1}-{2} is not a valid genome region. Please check the format of columns: {3}".format(line[chr], line[start], line[end], coordinate_columns))
+
+
+def format_adata_var(adata,
+                     coordinate_columns=None,
+                     columns_added=["chr", "start", "end"]):
+    '''
+    Formats the index of adata.var and adds peak_chr, peak_start, peak_end columns to adata.var if needed.
+    If coordinate_columns are given, the function will check if these columns already contain the information needed. If the coordinate_columns are in the correct format, nothing will be done.
+    If the coordinate_columns are invalid (or coordinate_columns is not given) the index is checked for the following format:
+    "*[_:-]start[_:-]stop"
+
+    If the index can be formatted, the formatted columns (columns_added) will be added.
+    If the index cannot be formatted, an error will be raised.
+
+    :param adata: AnnData
+        The anndata object containing features to annotate.
+    :param coordinate_columns: list of str or None
+        List of length 3 for column names in adata.var containing chr, start, end coordinates to check.
+        If None, the index will be formatted.
+    :param columns_added: list of str
+        List of length 3 for column names in adata.var containing chr, start, end coordinates to add.
+    '''
+
+    # Test whether the first three columns are in the right format
+    format_index = True
+    print(coordinate_columns)
+    if coordinate_columns is not None:
+        try:
+            validate_regions(adata, coordinate_columns)
+            format_index = False
+        except KeyError:
+            print("The coordinate columns are not found in adata.var. Trying to format the index.")
+        except ValueError:
+            print("The regions in adata.var are not in the correct format. Trying to format the index.")
+
+    # Format index if needed
+    if format_index:
+        print("formatting adata.var index to coordinate columns:")
+        regex = r'[^_:\-]+[\_\:\-]+[0-9]+[\_\:\-]+[0-9]+'  # matches chr_start_end / chr-start-end / chr:start-end and variations
+
+        # Prepare lists to insert
+        peak_chr_list = []
+        peak_start_list = []
+        peak_end_list = []
+
+        names = adata.var.index
+        for name in names:
+            if re.match(regex, name):  # test if name can be split by regex
+
+                # split the name into chr, start, end
+                split_name = re.split(r'[\_\:\-]', name)
+                peak_chr_list.append(split_name[0])
+                peak_start_list.append(int(split_name[1]))
+                peak_end_list.append(int(split_name[2]))
+
+            else:
+                raise ValueError("Index does not match the format *_start_stop or *:start-stop. Please check your index.")
+
+        adata.var.drop(columns_added, axis=1,
+                       errors='ignore', inplace=True)
+
+        adata.var.insert(0, columns_added[2], peak_end_list)
+        adata.var.insert(0, columns_added[1], peak_start_list)
+        adata.var.insert(0, columns_added[0], peak_chr_list)
+
+        # Check whether the newly added columns are in the right format
+        validate_regions(adata, columns_added)
+
+
 if __name__ == '__main__':
 
     # adata = epi.read_h5ad('/home/jan/python-workspace/sc-atac/processed_data/Esophagus/norm_correction/anndata/Esophagus.h5ad')
@@ -284,7 +481,7 @@ if __name__ == '__main__':
     qc_columns['UQ'] = 'UQ'
     qc_columns['CM'] = 'CM'
 
-    adata = assemble_from_h5ad(['/mnt/workspace/jdetlef/data/anndata/Esophagus.h5ad'], qc_columns, column='sample', from_snapatac=True, conditions=None)
+    adata = assemble_from_h5ad(['/mnt/agnerds/PROJECTS/extern/ext442_scATAC_Glaser_11_22/preprocessing_output/data/all_annotated_peaks.h5ad'], qc_columns, coordinate_cols=['peak_chr', 'peak_start', 'peak_end'], column='sample')
     # #adata = epi.read_h5ad('/mnt/workspace/jdetlef/processed_data/Esophagus/assembling/anndata/Esophagus.h5ad')
     #
     #
