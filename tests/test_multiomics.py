0d41c210f322981c3bb06c52fcf5a0f85c5a9ada
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2023-02-06 09:16:30 +0100

    Complete tests for merge_anndata()

--- tests/test_multiomics.py
+++ tests/test_multiomics.py
@@ -26,9 +26,27 @@ def adata2(adata):
 
 def test_merge_anndata(adata, adata2):
     """ Test if anndata are merged correctly """
-    merged_adata = multi.merge_anndata({"1": adata, "2": adata2})
+    adata_to_merge = {"1": adata, "2": adata2}
+    merged_adata = multi.merge_anndata(adata_to_merge)
 
+    new_obs_index = list(set(adata.obs.index) & set(adata2.obs.index))
+    new_obs_cols, new_obsm_entries, new_var_index = list(), list(), list()
+    new_var_cols = ["source"] 
+    for key, value in adata_to_merge.items():
+        new_var_cols += [f"{key}_{i}" for i in value.var.columns]
+        new_obs_cols += [f"{key}_{i}" for i in value.obs.columns]
+        new_obsm_entries += [f"X_{key}_{i.split('_')[-1]}" for i in list(value.obsm)]
+        new_var_index += [f"{key}_{i}" for i in value.var.index]
+
+    # Check if merged object is an AnnData object
     assert isinstance(merged_adata, anndata.AnnData)
-    assert list(merged_adata.obsm) == ['X_1_pca', 'X_1_umap', 'X_2_pca', 'X_2_umap']
-
-# ToDo test for var and obs columns
+    # Check if merged obsm entries are merged properly
+    assert all(elem in new_obsm_entries for elem in list(merged_adata.obsm))
+    # Check if merged var columns are merged properly
+    assert all(elem in new_var_cols for elem in merged_adata.var.columns)
+    # Check if merged obs columns are merged properly
+    assert all(elem in new_obs_cols for elem in merged_adata.obs.columns)
+    # Check if merged var index is merged properly
+    assert all(elem in new_var_index for elem in merged_adata.var.index)
+    # Check if merged obs index is merged properly
+    assert all(elem in new_obs_index for elem in merged_adata.obs.index)
