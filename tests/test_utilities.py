daf9b635d65c4e547d89855264919e0e59ae027a
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2023-02-09 15:42:16 +0100

    match fill_na test to updated function

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -25,16 +25,15 @@ def berries():
 @pytest.fixture
 def na_dataframe():
     data = {'int': [3, 2, 1, np.nan],
-            'float': [1.2, 3.4, 5.6, np.nan],
-            'string': ['a', 'b', 'c', np.nan],
-            'boolean': [True, False, True, np.nan],
-            'category_str': ['cat1', 'cat2', 'cat3', np.nan],
-            'category_num': [10, 20, 30, np.nan]}
+                'float': [1.2, 3.4, 5.6, np.nan],
+                'string': ['a', 'b', 'c', np.nan],
+                'boolean': [True, False, True, np.nan],
+                'category_str': ['cat1', 'cat2', 'cat3', np.nan],
+                'category_num': [10, 20, 30, np.nan]}
     df = pd.DataFrame.from_dict(data)
 
-    df['category_str'].astype('category')
-    df['category_num'].astype('category')
-
+    df['category_str'] = df['category_str'].astype('category')
+    df['category_num'] = df['category_num'].astype('category')
     return df
 
 
@@ -151,7 +150,7 @@ def test_fill_na(na_dataframe):
     """ Test if na values in dataframe are filled correctly """
     utils.fill_na(na_dataframe)
     assert not na_dataframe.isna().any().any()
-    assert list(na_dataframe.iloc[3, :]) == [0.0, 0.0, '-', False, '-', 0.0]
+    assert list(na_dataframe.iloc[3, :]) == [0.0, 0.0, '-', False, '', '']
 
 
 def test_get_adata_subsets(adata):
