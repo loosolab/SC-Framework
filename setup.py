864c9b81429a2469b7ba5832bcfb2eb8a2f18133
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2023-02-28 15:23:09 +0100

    Keep order of flattened list

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras_require = {"converter": ['rpy2', 'anndata2ri'],
                   "deseq2": ["rpy2", "diffexp @ git+https://github.com/wckdouglas/diffexpr.git@0bc0ba5e42712bfc2be17971aa838bcd7b27a785#egg=diffexp"]  # rpy2 must be installed before diffexpr
                   }
 
-extras_require["all"] = list(set(sum(extras_require.values(), [])))  # flatten list of all requirements
+extras_require["all"] = list(dict.fromkeys([item for sublist in extras_require.values() for item in sublist]))  # flatten list of all requirements
 
 setup(
     name='sc-toolbox',
