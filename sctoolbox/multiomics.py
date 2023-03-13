be539632d5a1ec0688dfe5608d7f3d8e8a27e340
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2023-03-13 12:54:17 +0100

    Applied suggestions to create NEW minimal adata dict

--- sctoolbox/multiomics.py
+++ sctoolbox/multiomics.py
@@ -19,7 +19,7 @@ def merge_anndata(anndata_dict, join="inner"):
     ----------
     anndata_dict : dict
         dictionary with labels as keys and anndata objects as values
-    join : string, deafult 'inner'
+    join : str, deafult 'inner'
         set how to join cells of the adata objects: ['inner', 'outer']
         This only affects the cells since the var/gene section is simply added
         'inner': only keep overlapping cells
@@ -38,17 +38,16 @@ def merge_anndata(anndata_dict, join="inner"):
         raise ValueError(f"Invalid join value: {join}. Set to 'inner' or 'outer'")
 
     # Generate minimal anndata objects
+    minimal_adata_dict = dict()
     for label, adata in anndata_dict.items():
-
+        minimal_adata_dict[label] = ad.AnnData(X=adata.X, obs=adata.obs, var=adata.var, obsm=dict(adata.obsm))
         if not adata.obs.index.is_unique:
             warnings.warn(f"Obs index of {label} dataset is not unqiue. Running .obs_names_make_unique()..")
-            adata.obs_names_make_unique()
+            minimal_adata_dict[label].obs_names_make_unique()
 
         if not adata.var.index.is_unique:
             warnings.warn(f"Var index of {label} dataset is not unqiue. Running .var_names_make_unique()..")
-            adata.var_names_make_unique()
-
-        anndata_dict[label] = ad.AnnData(X=adata.X, obs=adata.obs, var=adata.var, obsm=adata.obsm)
+            minimal_adata_dict[label].var_names_make_unique()
 
     # Get cell barcode (obs) intersection
     all_obs_indices = [single_adata.obs.index for single_adata in list(anndata_dict.values())]
@@ -59,42 +58,35 @@ def merge_anndata(anndata_dict, join="inner"):
     obsm_dict = dict()
 
     # Add prefix to obsm dict keys, var index and obs columns
-    for label, adata in anndata_dict.items():
-
-        # prefix to obsm
-        adata.obsm = {f"X_{label}_{key.removeprefix('X_')}": val for key, val in adata.obsm.items()}
+    for label, adata in minimal_adata_dict.items():
         # prefix to var index
         adata.var.index = label + "_" + adata.var.index
         adata.var.columns = label + "_" + adata.var.columns
         # prefix to obs columns
         adata.obs.columns = label + "_" + adata.obs.columns
-
         # Reorder adata cells
         adata = adata[obs_intersection, :]
-
-        for obsm_key, matrix in adata.obsm.items():
-            obsm_dict[obsm_key] = matrix
-
+        for obsm_key, matrix in dict(adata.obsm).items():
+            obsm_dict[f"X_{label}_{obsm_key.removeprefix('X_')}"] = matrix
         # save new adata to dict
-        anndata_dict[label] = adata
+        minimal_adata_dict[label] = adata
         # save obs in list
         obs_list.append(adata.obs)
-
     # Merge X and var
-    merged_X_var = ad.concat(anndata_dict, join="outer", label="source", axis=1)
+    merged_adata = ad.concat(minimal_adata_dict, join="outer", label="source", axis=1)
 
     # Merge obs
-    merged_X_var.obs = reduce(lambda left, right: pd.merge(left, right,
+    merged_adata.obs = reduce(lambda left, right: pd.merge(left, right,
                                                            how=join,
                                                            left_index=True,
                                                            right_index=True), obs_list)
-    merged_X_var.obsm = obsm_dict
+    merged_adata.obsm = obsm_dict
 
-    utils.fill_na(merged_X_var.obs)
-    utils.fill_na(merged_X_var.var)
+    utils.fill_na(merged_adata.obs)
+    utils.fill_na(merged_adata.var)
 
-    if len(merged_X_var.var) <= 50:
+    if len(merged_adata.var) <= 50:
         warnings.warn("The adata object contains less than 51 genes/var entries. "
                       + "CellxGene will not work. Please add dummy genes to the var table.")
 
-    return merged_X_var
+    return merged_adata
