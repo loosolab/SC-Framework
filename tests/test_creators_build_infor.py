a28a371810fab1509a788a2426366288b52b6dd3
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2023-03-20 14:56:45 +0100

    Added test for gitlab_download()

--- tests/test_creators_build_infor.py
+++ tests/test_creators_build_infor.py
@@ -5,6 +5,7 @@ import pandas as pd
 from glob import glob
 from pathlib import Path
 import shutil
+from unittest.mock import patch, MagicMock, Mock
 
 
 @pytest.fixture
@@ -129,8 +130,39 @@ def test_setup_experiment():
     assert set([Path(file).name for file in f]) == set(dirs)
 
 
-def test_add_analysis():
-    pass
+def test_gitlab_download():
+    """Test gitlab download"""
+    def side_effect(search):
+
+        def repo_tree(path, ref):
+            return_value = [{'name': 'Notebook1.ipynb', 'type': 'blob', 'path': 'notebooks/Notebook1.ipynb'},
+                            {'name': 'FileX.py', 'type': 'blob', 'path': 'notebooks/FileX.py'}]
+            return return_value
+
+        mock2 = Mock()
+        mock2.name = search
+        mock2.repository_tree = repo_tree
+        return [mock2]
+
+    mock = Mock()
+    mock.projects.list = side_effect
+    result_file = Path("./tmp-test_add_analysis/Notebook1.ipynb")
+    missing_file = Path("./tmp-test_add_analysis/FileX.py")
+    result_file.parent.mkdir(parents=True, exist_ok=True)
+    try:
+        with patch("gitlab.Gitlab", return_value=mock):
+                gitlab_download("notebooks", file_regex=".*.ipynb", out_path = "./tmp-test_add_analysis/")
+                assert result_file.is_file()
+                assert not missing_file.is_file()
+    except Exception as e:
+        print(e)
+    finally:
+        if result_file.is_file():
+            result_file.unlink()
+        if missing_file.is_file():
+            missing_file.unlink()
+        if result_file.parent.exists():
+            result_file.parent.rmdir()
 
 
 @pytest.mark.parametrize("starts,regex", [(1, '[0]*[1-9].*.ipynb'), (5, '[0]*[5-9].*.ipynb'),
