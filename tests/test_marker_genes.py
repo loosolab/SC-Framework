a820ce0ba40865687beceb76ddbe6848e5586f0b
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2023-05-12 10:56:23 +0200

    typo

--- tests/test_marker_genes.py
+++ tests/test_marker_genes.py
@@ -103,7 +103,7 @@ def test_mask_rank_genes(adata):
 @pytest.mark.parametrize(
     "score_name, gene_set, inplace",
     [
-        ("test1", pytest.lazy_ficture("gene_set"), False), # https://stackoverflow.com/a/64348247/19870975
+        ("test1", pytest.lazy_fixture("gene_set"), False), # https://stackoverflow.com/a/64348247/19870975
         ("test2", os.path.join(os.path.dirname(__file__), 'data', 'test_score_genes.txt'), True)
     ]
 )
