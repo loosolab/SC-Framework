7f670247a64da447b8ffade8e23b7c91a19807a8
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2023-05-22 12:44:55 +0200

    Fix notebooks run

--- scripts/run_notebooks.sh
+++ scripts/run_notebooks.sh
@@ -1,6 +1,5 @@
 cd rna-notebooks
-papermill --log-output -k sctoolbox --log-level DEBUG 01_assembling_anndata.ipynb out.ipynb
-echo "finished running notebook 1"
-papermill --log-output -k sctoolbox --log-level DEBUG 02_QC_filtering.ipynb out.ipynb
-papermill --log-output -k sctoolbox --log-level DEBUG 03_batch.ipynb out.ipynb
-papermill --log-output -k sctoolbox --log-level DEBUG 04_clustering.ipynb out.ipynb
+papermill --log-output -k sctoolbox --log-level INFO 01_assembling_anndata.ipynb out.ipynb
+papermill --log-output -k sctoolbox --log-level INFO 02_QC_filtering.ipynb out.ipynb
+papermill --log-output -k sctoolbox --log-level INFO 03_batch.ipynb out.ipynb
+papermill --log-output -k sctoolbox --log-level INFO 04_clustering.ipynb out.ipynb
