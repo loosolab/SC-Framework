24e02fe8512f4b1f7213979a5c57f52755dffaee
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2023-05-22 13:45:34 +0200

    Get cwd from notebook runner

--- scripts/run_notebooks.sh
+++ scripts/run_notebooks.sh
@@ -1,5 +1,12 @@
-cd rna-notebooks
-papermill --log-output -k sctoolbox --log-level INFO 01_assembling_anndata.ipynb out.ipynb
-papermill --log-output -k sctoolbox --log-level INFO 02_QC_filtering.ipynb out.ipynb
-papermill --log-output -k sctoolbox --log-level INFO 03_batch.ipynb out.ipynb
-papermill --log-output -k sctoolbox --log-level INFO 04_clustering.ipynb out.ipynb
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
+
+# Run RNA notebooks
+cd $SCRIPT_DIR/../rna-notebooks
+papermill -k sctoolbox --log-level INFO 01_assembling_anndata.ipynb out.ipynb
+#papermill --log-output -k sctoolbox --log-level INFO 01_assembling_anndata.ipynb out.ipynb
+papermill -k sctoolbox --log-level INFO 02_QC_filtering.ipynb out.ipynb
+papermill -k sctoolbox --log-level INFO 03_batch.ipynb out.ipynb
+papermill -k sctoolbox --log-level INFO 04_clustering.ipynb out.ipynb
