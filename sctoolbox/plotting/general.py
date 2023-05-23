3fc838ef40d49da83210864a5757b25fbe404ffd
Micha-Frederick Kessler <micha-frederick.kessler@mpi-bn.mpg.de>
2023-05-23 09:33:48 +0000

    updated violinplot example

--- sctoolbox/plotting/general.py
+++ sctoolbox/plotting/general.py
@@ -537,18 +537,17 @@ def violinplot(table, y, color_by=None, hlines=None, colors=None, ax=None, title
 
         import sctoolbox.plotting as pl
         import matplotlib.pyplot as plt
-        import pandas as pd
-        import numpy as np
+        import seaborn as sns
 
     .. plot::
         :context: close-figs
 
-        table = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
+        table = sns.load_dataset("titanic")
 
     .. plot::
         :context: close-figs
         
-        pl.violinplot(table, "A", color_by=None, hlines=None, colors=None, ax=None, title=None, ylabel=True)
+        pl.violinplot(table, "age", color_by="class", hlines=None, colors=None, ax=None, title=None, ylabel=True)
         plt.show()
     """
 
