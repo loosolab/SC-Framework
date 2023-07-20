df2b94f1d1f9b3b5d9c04607f2a7c8bfbf0b4dfa
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2023-07-20 15:23:30 +0200

    adjust log tests

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -2,6 +2,7 @@ import os
 import scanpy as sc
 import pytest
 from sctoolbox.utils import decorator as deco
+import sctoolbox.utils as utils
 
 
 @pytest.fixture
@@ -29,12 +30,29 @@ def test_log_anndata(adata):
 
     # check if log is in adata.uns
     assert "sctoolbox" in adata.uns
-    assert "test_func" in adata.uns["sctoolbox"]
+    assert "log" in adata.uns["sctoolbox"]
+    assert "test_func" in adata.uns["sctoolbox"]["log"]
     # check if run_1 and run_2 are in adata.uns
-    assert adata.uns["sctoolbox"]["test_func"]["run_1"]['kwargs']["param1"] == 1
-    assert adata.uns["sctoolbox"]["test_func"]["run_1"]['kwargs']["param2"] is None
-    assert adata.uns["sctoolbox"]["test_func"]["run_1"]['kwargs']["param3"] == "test"
-    assert adata.uns["sctoolbox"]["test_func"]["run_1"]['kwargs']["param4"] == 1.0
-    assert adata.uns["sctoolbox"]["test_func"]["run_1"]['kwargs']["param5"] is True
+    assert adata.uns["sctoolbox"]["log"]["test_func"]["run_1"]['kwargs']["param1"] == 1
+    assert adata.uns["sctoolbox"]["log"]["test_func"]["run_1"]['kwargs']["param2"] is None
+    assert adata.uns["sctoolbox"]["log"]["test_func"]["run_1"]['kwargs']["param3"] == "test"
+    assert adata.uns["sctoolbox"]["log"]["test_func"]["run_1"]['kwargs']["param4"] == 1.0
+    assert adata.uns["sctoolbox"]["log"]["test_func"]["run_1"]['kwargs']["param5"] is True
 
-    assert adata.uns["sctoolbox"]["test_func"]["run_2"]['kwargs']["param1"] == 1
+    assert adata.uns["sctoolbox"]["log"]["test_func"]["run_2"]['kwargs']["param1"] == 1
+
+
+def test_add_uns_info(adata):
+    """ Test if add_uns_info works on both string and list keys. """
+
+    utils.add_uns_info(adata, "akey", "info")
+    print(adata.uns)
+    assert "akey" in adata.uns["sctoolbox"]
+    assert adata.uns["sctoolbox"]["akey"] == "info"
+
+    utils.add_uns_info(adata, ["upper", "lower"], "info")
+    assert "upper" in adata.uns["sctoolbox"]
+    assert adata.uns["sctoolbox"]["upper"]["lower"] == "info"
+
+    utils.add_uns_info(adata, ["upper", "lower"], "info2", how="append")
+    assert adata.uns["sctoolbox"]["upper"]["lower"] == ["info", "info2"]
