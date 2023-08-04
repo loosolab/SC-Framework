42e4da9ca1cf967479aff484cb029ca7816cd056
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2023-08-04 16:02:00 +0200

    tools/highly_variable.py: Doc-string linting

--- sctoolbox/tools/highly_variable.py
+++ sctoolbox/tools/highly_variable.py
@@ -17,8 +17,6 @@ def annot_HVG(anndata, min_mean=0.0125, max_iterations=10, hvg_range=(1000, 5000
     """
     Annotate highly variable genes (HVG). Tries to annotate in given range of HVGs, by gradually in-/ decreasing min_mean of scanpy.pp.highly_variable_genes.
 
-    Note: Logarithmized data is expected.
-
     Parameters
     ----------
     anndata : anndata.AnnData
@@ -38,6 +36,10 @@ def annot_HVG(anndata, min_mean=0.0125, max_iterations=10, hvg_range=(1000, 5000
         Path to save the plot to. If None, the plot is not saved.
     **kwargs :
         Additional arguments forwarded to scanpy.pp.highly_variable_genes().
+
+    Notes
+    -----
+    Logarithmized data is expected.
 
     Returns
     -------
@@ -80,27 +82,33 @@ def annot_HVG(anndata, min_mean=0.0125, max_iterations=10, hvg_range=(1000, 5000
         return adata_m
 
 
-# This is for ATAC-seq data
 @deco.log_anndata
 def get_variable_features(adata, max_cells=None, min_cells=None, show=True, inplace=True):
     """
     Get the highly variable features of anndata object. Adds the column "highly_variable" to adata.var. If show is True, the plot is shown.
 
     Parameters
-    -----------
+    ----------
     adata : anndata.AnnData
         The anndata object containing counts for variables.
-    min_score : float, optional
-        The minimum variability score to set as threshold. Default: None (automatic)
-    show : bool
-        Show plot of variability scores and thresholds. Default: True.
-    inplace : bool
-        If True, the anndata object is modified. Otherwise, a new anndata object is returned. Default: True.
+    max_cells : float, default None
+        The maximum variability score to set as threshold.
+    min_cells : float, default None
+        The minimum variability score to set as threshold.
+    show : bool, default True
+        Show plot of variability scores and thresholds.
+    inplace : bool, default True
+        If True, the anndata object is modified. Otherwise, a new anndata object is returned.
+
+    Notes
+    -----
+    Designed for scATAC-seq data
 
     Returns
-    --------
-    If inplace is False, the function returns None
-    If inplace is True, the function returns an anndata object.
+    -------
+    anndata.AnnData or None
+        If inplace is False, the function returns None
+        If inplace is True, the function returns an anndata object.
     """
     utils.check_module("kneed")
     utils.check_module("statsmodels")
