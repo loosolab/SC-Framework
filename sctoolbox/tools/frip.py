6233a5f02e85db6a0928ba223ebea86dde1b9926
mette.bentsen <mette.bentsen@mpi-bn.mpg.de>
2023-11-20 13:15:50 +0100

    Fix merge

--- sctoolbox/tools/frip.py
+++ sctoolbox/tools/frip.py
@@ -9,10 +9,6 @@ import sctoolbox.utils.decorator as deco
 from sctoolbox._settings import settings
 
 from beartype import beartype
-<<<<<<< HEAD
-=======
-
->>>>>>> dev
 from beartype.typing import Tuple
 logger = settings.logger
 
