9f3bdb03bd5b71441e2f84154227d682f737ddf0
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2023-11-30 09:35:35 +0100

    Updated rna-notebook path for general notebook runs

--- scripts/run_notebooks.py
+++ scripts/run_notebooks.py
@@ -5,9 +5,10 @@ import papermill as pm
 # Get location of script
 script_dir = os.path.dirname(__file__)
 print(script_dir)
+rna_notebook_path_suffix = "/../rna_analysis/notebooks/"
 
 # Run RNA notebooks
-notebook_dir = script_dir + "/../rna_analysis/notebooks/"
+notebook_dir = script_dir + rna_notebook_path_suffix
 rna_notebooks = sorted(glob.glob(notebook_dir + "*.ipynb"))  # sort as glob output is not ordered
 rna_notebooks = [nb for nb in rna_notebooks if "05" not in nb and "11" not in nb]
 print(rna_notebooks)
@@ -36,4 +37,4 @@ print(general_notebooks)
 
 for notebook in general_notebooks:
     print(f"Running notebook: {notebook}")
-    pm.execute_notebook(notebook, output_path="out.ipynb", kernel_name='sctoolbox', log_level="INFO", report_mode=True, cwd=script_dir + "/../rna-notebooks/")
+    pm.execute_notebook(notebook, output_path="out.ipynb", kernel_name='sctoolbox', log_level="INFO", report_mode=True, cwd=script_dir + rna_notebook_path_suffix)
