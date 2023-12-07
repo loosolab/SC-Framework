c73556729044cdc903c5e9dab70a741022e3fcea
Vanessa Heger <vanessa.heger@mpi-bn.mpg.de>
2023-12-07 13:35:25 +0000

    small bug fix

--- sctoolbox/plotting/general.py
+++ sctoolbox/plotting/general.py
@@ -730,16 +730,16 @@ def plot_venn(groups_dict: dict[str, list[Any]],
         venn2 = { 'Group A': {1, 2, 3, 4},
                   'Group B': {3, 4, 5, 6}
                 }
-        venn_fruits = { 'Fruits A': {'Lemon', 'Orange', Blueberry, 'Grapefruit'},
-                        'Fruits B': {'Pineapple', 'Mango', 'Banana', 'Papaya', Blueberry, 'Strawberry'},
+        venn_fruits = { 'Fruits A': {'Lemon', 'Orange', 'Blueberry', 'Grapefruit'},
+                        'Fruits B': {'Pineapple', 'Mango', 'Banana', 'Papaya', 'Blueberry', 'Strawberry'},
                         'Fruits C': {'Strawberry', 'Blueberry', 'Raspberry', 'Orange', 'Mango'}
                     }
 
     .. plot::
         :context: close-figs
 
-        pl.venn2(venn2, title = "Simple Venn2 plot")
-        pl.venn2(venn3, title = "Simple Venn3 plot")
+        pl.venn2(venn2, "Simple Venn2 plot")
+        pl.venn2(venn3, "Simple Venn3 plot")
 
     """
 
