5c6455c0ea35971fb44e42289970b151c063404d
Sweta Talyan <sweta.talyan@mpi-bn.mpg.de>
2024-02-19 14:13:08 +0000

    Update clustering.py

--- sctoolbox/tools/clustering.py
+++ sctoolbox/tools/clustering.py
@@ -61,12 +61,12 @@ def recluster(adata: sc.AnnData,
     ------
     .. plot::
         :context: close-figs
-
+        adata = sc.datasets.pbmc68k_reduced()
         sctoolbox.tools.recluster(adata, column="louvain", clusters=["1", "2"], task="join", method="leiden", resolution=1.5, plot=True, embedding=None)
 
     .. plot::
         :context: close-figs
-
+        adata = sc.datasets.pbmc68k_reduced()
         sctoolbox.tools.recluster(adata,column="louvain", clusters=["3", "4"], task="split", method="leiden", resolution=1.5, plot=True, embedding=None)
     """
 
