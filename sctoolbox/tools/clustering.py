7ae5bd22f01bce8ed66ab1bae7a6c087edcf2b23
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2024-02-26 09:09:24 +0100

    embedding can not be None

--- sctoolbox/tools/clustering.py
+++ sctoolbox/tools/clustering.py
@@ -64,16 +64,18 @@ def recluster(adata: sc.AnnData,
 
         import scanpy as sc
         import sctoolbox
+
         adata = sc.datasets.pbmc68k_reduced()
-        sctoolbox.tools.recluster(adata, column="louvain", clusters=["1", "2"], task="join", method="leiden", resolution=1.5, plot=True, embedding=None)
+        sctoolbox.tools.recluster(adata, column="louvain", clusters=["1", "2"], task="join", method="leiden", resolution=1.5, plot=True)
 
     .. plot::
         :context: close-figs
 
         import scanpy as sc
         import sctoolbox
+
         adata = sc.datasets.pbmc68k_reduced()
-        sctoolbox.tools.recluster(adata,column="louvain", clusters=["3", "4"], task="split", method="leiden", resolution=1.5, plot=True, embedding=None)
+        sctoolbox.tools.recluster(adata, column="louvain", clusters=["3", "4"], task="split", method="leiden", resolution=1.5, plot=True)
     """
 
     adata_copy = adata.copy()
