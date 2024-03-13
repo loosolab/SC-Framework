7b49ef10d1b54f314e51b6b30a0b49eb5193eda0
Sweta.Talyan <Sweta.Talyan@mpi-bn.mpg.de>
2024-03-13 13:48:00 +0100

    added test for starsolo qc

---
+++ tests/plotting/test_qc_filter.py
@@ -0,0 +1,61 @@
+"""Test qc_filter plotting function"""
+
+import pytest
+import sctoolbox.plotting as pl
+import scanpy as sc
+import os
+import tempfile
+import shutil
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import ipywidgets as widgets
+import functools
+import matplotlib.pyplot as plt
+import glob
+
+from beartype.roar import BeartypeCallHintParamViolation
+
+
+
+@pytest.mark.parametrize("order", [None, ["KO-2", "KO-1", "Ctrl-2", "Ctrl-1"]])
+def test_plot_starsolo_quality(order):
+    """Test plot_starsolo_quality success."""
+    res = pl.plot_starsolo_quality(quant_folder, order=order)
+
+    assert isinstance(res, np.ndarray)
+
+
+def test_plot_starsolo_quality_failure():
+    """Test plot_starsolo_quality failure with invalid input."""
+
+    with pytest.raises(ValueError, match="No STARsolo summary files found in folder*"):
+        pl.plot_starsolo_quality("invalid")
+
+    with pytest.raises(KeyError, match="Measure .* not found in summary table"):
+        pl.plot_starsolo_quality(quant_folder, measures=["invalid"])
+        
+
+def test_plot_starsolo_UMI():
+    """Test plot_starsolo_UMI success."""
+    res = pl.plot_starsolo_UMI(quant_folder)
+
+    assert isinstance(res, np.ndarray)
+
+
+def test_plot_starsolo_UMI_failure():
+    """Test plot_starsolo_UMI failure with invalid input."""
+
+    # Create a quant folder without UMI files
+    shutil.copytree(quant_folder, "quant_without_UMI", dirs_exist_ok=True)
+    UMI_files = glob.glob("quant_without_UMI/*/solo/Gene/UMI*")
+    for file in UMI_files:
+        os.remove(file)
+
+    # Test that valueerror is raised
+    with pytest.raises(ValueError, match="No UMI files found in folder*"):
+        pl.plot_starsolo_UMI("quant_without_UMI")
+
+    # remove folder
+    shutil.rmtree("quant_without_UMI")
+
