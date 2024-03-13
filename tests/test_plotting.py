6008a909e9977935398824a507b165d82d3cf491
Aviral Jain <aviral.jain@mpi-bn.mpg.de>
2024-03-13 13:15:33 +0000

    Update file test_plotting.py

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -448,14 +448,6 @@ def test_group_expression_boxplot(adata):
     assert ax_type.startswith("Axes")
 
 
-def test_boxplot(df):
-    """Test if Axes object is returned."""
-    ax = pl.boxplot(df)
-    ax_type = type(ax).__name__
-
-    assert ax_type.startswith("Axes")
-
-
 @pytest.mark.parametrize("color", ["ENSMUSG00000102693", "clustering", "qc_float"])
 def test_plot_3D_UMAP(adata, color):
     """Test if 3d plot is written to html."""
@@ -654,76 +646,6 @@ def test_add_labels(df, label):
     texts = pl._add_labels(df, x="col1", y="col2", label_col=label)
     assert isinstance(texts, list)
     assert type(texts[0]).__name__ == "Annotation"
-
-
-def test_clustermap_dotplot():
-    """Test clustermap_dotplot success."""
-    table = sc.datasets.pbmc68k_reduced().obs.reset_index()[:10]
-    axes = pl.clustermap_dotplot(table=table, x="bulk_labels",
-                                 y="index", hue="n_genes",
-                                 size="n_counts", palette="viridis",
-                                 title="Title", show_grid=True)
-
-    assert isinstance(axes, list)
-    ax_type = type(axes[0]).__name__
-    assert ax_type.startswith("Axes")
-
-
-def test_bidirectional_barplot(df_bidir_bar):
-    """Test bidirectoional_barplot success."""
-    pl.bidirectional_barplot(df_bidir_bar, title="Title")
-    assert True
-
-
-def test_bidirectional_barplot_fail(df):
-    """Test bidorectional_barplot with invalid input."""
-    with pytest.raises(KeyError, match='Column left_label not found in dataframe.'):
-        pl.bidirectional_barplot(df)
-
-
-@pytest.mark.parametrize("ylabel,color_by,hlines", [(True, None, 0.5),
-                                                    (False, "clustering", [0.5, 0.5, 0.5, 0.5])])
-def test_violinplot(adata, ylabel, color_by, hlines):
-    """Test violinplot success."""
-    ax = pl.violinplot(adata.obs, "qc_float", color_by=color_by,
-                       hlines=hlines, colors=None, ax=None,
-                       title="Title", ylabel=ylabel)
-    ax_type = type(ax).__name__
-    assert ax_type.startswith("Axes")
-
-
-def test_violinplot_fail(adata):
-    """Test invalid input for violinplot."""
-    with pytest.raises(ValueError, match='not found in column names of table!'):
-        pl.violinplot(adata.obs, y="Invalid")
-
-    with pytest.raises(ValueError, match='Color grouping'):
-        pl.violinplot(adata.obs, y="qc_float", color_by="Invalid")
-
-    with pytest.raises(ValueError, match='Parameter hlines has to be number or list'):
-        pl.violinplot(adata.obs, y="qc_float", hlines={"A": 0.5})
-
-    with pytest.raises(ValueError, match='Invalid dict keys in hlines parameter.'):
-        pl.violinplot(adata.obs, y="qc_float",
-                      color_by="clustering", hlines={"A": 0.5})
-
-
-def test_plot_venn(venn_dict):
-    """Test plot_venn with 3 and 2 groups."""
-    pl.plot_venn(venn_dict, title="Test")
-    venn_dict.pop("Group C")
-    pl.plot_venn(venn_dict, title="Test")
-    assert True
-
-
-def test_plot_venn_fail(venn_dict):
-    """Test for invalid input."""
-    venn_dict["Group D"] = [1, 2]
-    with pytest.raises(ValueError):
-        pl.plot_venn(venn_dict)
-
-    with pytest.raises(BeartypeCallHintParamViolation):
-        pl.plot_venn([1, 2, 3, 4, 5])
 
 
 def test_violin_HVF_distribution(adata):
@@ -944,26 +866,6 @@ def test_get_slider_thresholds_dict_grouped_diff(slider_dict_grouped_diff):
                                     '2': {'min': 1, 'max': 5}},
                               'B': {'1': {'min': 5, 'max': 7},
                                     '2': {'min': 3, 'max': 4}}}
-
-
-@pytest.mark.parametrize("columns", [["invalid"], ["not", "present"]])
-def test_pairwise_scatter_invalid(adata, columns):
-    """Test that invalid columns raise error."""
-    with pytest.raises(ValueError):
-        pl.pairwise_scatter(adata.obs, columns=columns)
-
-    with pytest.raises(BeartypeCallHintParamViolation):
-        pl.pairwise_scatter(adata.obs, columns="invalid")
-
-
-@pytest.mark.parametrize("thresholds", [None,
-                                        {"qcvar1": {"min": 0.1}, "qcvar2": {"min": 0.4}}])
-def test_pairwise_scatter(adata, thresholds):
-    """Test pairwise scatterplot with different input."""
-    axarr = pl.pairwise_scatter(adata.obs, columns=["qcvar1", "qcvar2"], thresholds=thresholds)
-
-    assert axarr.shape == (2, 2)
-    assert type(axarr[0, 0]).__name__.startswith("Axes")
 
 
 @pytest.mark.parametrize("order", [None, ["KO-2", "KO-1", "Ctrl-2", "Ctrl-1"]])
