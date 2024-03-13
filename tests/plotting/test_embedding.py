83ab9ea980f4dfa77fa359d6078a62ced3616959
Yousef Alayoubi <yousef.alayoubi@mpi-bn.mpg.de>
2024-03-13 14:19:39 +0100

    linting

--- tests/plotting/test_embedding.py
+++ tests/plotting/test_embedding.py
@@ -8,11 +8,7 @@ import tempfile
 import shutil
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import ipywidgets as widgets
-import functools
 import matplotlib.pyplot as plt
-import glob
 
 from beartype.roar import BeartypeCallHintParamViolation
 
