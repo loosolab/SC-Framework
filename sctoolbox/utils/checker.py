078ef52b7b527b19f59b81b34aaa5960278dda64
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2024-04-02 15:02:41 +0200

    Update example

--- sctoolbox/utils/checker.py
+++ sctoolbox/utils/checker.py
@@ -391,8 +391,9 @@ def in_range(value: int | float, limits: Tuple[int | float, int | float],
     Examples
     --------
     .. exec_code::
-
-        in_range(value=0.5, limits=(0.5, 1), include_limits=True)
+        limit = (0.5, 1)
+        value = 0.5
+        print(utils.in_range(value=value, limits=limit, include_limits=True))
 
     This will return 'True'; the value is in between the limits including the limits.
     """
