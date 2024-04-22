13879c219669af69a1e3308b0a7a055db5bc5c8c
Sweta.Talyan <Sweta.Talyan@mpi-bn.mpg.de>
2024-04-22 14:41:35 +0200

    tests have been removed from the test_plotting.py file

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -464,32 +464,6 @@ def test_invalid_color_plot_3D_UMAP(adata):
         pl.plot_3D_UMAP(adata, color="invalid", save="3D_test")
 
 
-def test_group_correlation(adata):
-    """Test if plot is written to pdf."""
-
-    # Run group correlation
-    pl.group_correlation(adata, groupby="condition",
-                         save="group_correlation.pdf")
-
-    # Assert creation of file
-    assert os.path.isfile("group_correlation.pdf")
-    os.remove("group_correlation.pdf")
-
-
-@pytest.mark.parametrize("groupby", [None, "condition"])
-@pytest.mark.parametrize("add_labels", [True, False])
-def test_n_cells_barplot(adata, groupby, add_labels):
-    """Test n_cells_barplot success."""
-
-    axarr = pl.n_cells_barplot(adata, "clustering", groupby=groupby,
-                               add_labels=add_labels)
-
-    if groupby is None:
-        assert len(axarr) == 1
-    else:
-        assert len(axarr) == 2
-
-
 @pytest.mark.parametrize("show_umap", [True, False])
 def test_marker_gene_clustering(adata, show_umap):
     """Test marker_gene_clustering."""
@@ -717,71 +691,6 @@ def test_scatter_HVF_distribution_fail(adata):
         pl.scatter_HVF_distribution(adata)
 
 
-def test_link_sliders(slider_list):
-    """Test _link_sliders success."""
-    linkage_list = pl._link_sliders(slider_list)
-    assert isinstance(linkage_list, list)
-    assert type(linkage_list[0]).__name__ == 'link'
-
-
-@pytest.mark.parametrize("global_threshold", [True, False])
-def test_toggle_linkage(checkbox, slider_list, global_threshold):
-    """Test if toggle_linkage runs without error."""
-    column = "Test"
-    linkage_dict = dict()
-    linkage_dict[column] = pl._link_sliders(slider_list) if global_threshold is True else None
-    checkbox.observe(functools.partial(pl._toggle_linkage,
-                                       linkage_dict=linkage_dict,
-                                       slider_list=slider_list,
-                                       key=column), names=["value"])
-    assert True
-
-
-def test_update_threshold(slider):
-    """Test if update_threshold runs wihtout error."""
-    fig, _ = plt.subplots()
-    slider.observe(functools.partial(pl._update_thresholds, fig=fig,
-                                     min_line=1, min_shade=1,
-                                     max_line=1, max_shade=1),
-                   names=["value"])
-    assert True
-
-
-@pytest.mark.parametrize("columns, which, groupby", [(['qc_float', 'LISI_score_pca'], "obs", "condition"),
-                                                     (['qc_float', 'LISI_score_pca'], "obs", "cat"),
-                                                     (['qc_float_var'], "var", None)])
-@pytest.mark.parametrize("color_list", [None, sns.color_palette("Set1", 3)])
-@pytest.mark.parametrize("title", [None, "Title"])
-def test_quality_violin(adata, groupby, columns, which, title, color_list):
-    """Test quality_violin success."""
-    figure, slider = pl.quality_violin(adata, columns=columns, groupby=groupby,
-                                       which=which, title=title, color_list=color_list)
-    assert type(figure).__name__ == "Figure"
-    assert isinstance(slider, dict)
-
-
-def test_quality_violin_fail(adata):
-    """Test quality_violin failure."""
-    with pytest.raises(BeartypeCallHintParamViolation):
-        pl.quality_violin(adata, columns=["qc_float"], which="Invalid")
-    with pytest.raises(ValueError, match="Increase the color_list variable"):
-        pl.quality_violin(adata, groupby="condition", columns=["qc_float"],
-                          color_list=sns.color_palette("Set1", 1))
-    with pytest.raises(ValueError, match="Length of header does not match"):
-        pl.quality_violin(adata, groupby="condition", columns=["qc_float"],
-                          header=[])
-    with pytest.raises(ValueError, match="The following columns from 'columns' were not found"):
-        pl.quality_violin(adata, columns=["Invalid"])
-
-
-def test_get_slider_thresholds_dict(slider_dict):
-    """Test get_slider_threshold for non grouped slider_dict."""
-    threshold_dict = pl.get_slider_thresholds(slider_dict)
-    assert isinstance(threshold_dict, dict)
-    assert threshold_dict == {'LISI_score_pca': {'min': 5, 'max': 7},
-                              'qc_float': {'min': 5, 'max': 7}}
-
-
 def test_get_slider_thresholds_dict_grouped(slider_dict_grouped):
     """Test get_slider_threshold for grouped slider_dict."""
     threshold_dict = pl.get_slider_thresholds(slider_dict_grouped)
@@ -818,45 +727,3 @@ def test_pairwise_scatter(adata, thresholds):
 
     assert axarr.shape == (2, 2)
     assert type(axarr[0, 0]).__name__.startswith("Axes")
-
-
-@pytest.mark.parametrize("order", [None, ["KO-2", "KO-1", "Ctrl-2", "Ctrl-1"]])
-def test_plot_starsolo_quality(order):
-    """Test plot_starsolo_quality success."""
-    res = pl.plot_starsolo_quality(quant_folder, order=order)
-
-    assert isinstance(res, np.ndarray)
-
-
-def test_plot_starsolo_quality_failure():
-    """Test plot_starsolo_quality failure with invalid input."""
-
-    with pytest.raises(ValueError, match="No STARsolo summary files found in folder*"):
-        pl.plot_starsolo_quality("invalid")
-
-    with pytest.raises(KeyError, match="Measure .* not found in summary table"):
-        pl.plot_starsolo_quality(quant_folder, measures=["invalid"])
-
-
-def test_plot_starsolo_UMI():
-    """Test plot_starsolo_UMI success."""
-    res = pl.plot_starsolo_UMI(quant_folder)
-
-    assert isinstance(res, np.ndarray)
-
-
-def test_plot_starsolo_UMI_failure():
-    """Test plot_starsolo_UMI failure with invalid input."""
-
-    # Create a quant folder without UMI files
-    shutil.copytree(quant_folder, "quant_without_UMI", dirs_exist_ok=True)
-    UMI_files = glob.glob("quant_without_UMI/*/solo/Gene/UMI*")
-    for file in UMI_files:
-        os.remove(file)
-
-    # Test that valueerror is raised
-    with pytest.raises(ValueError, match="No UMI files found in folder*"):
-        pl.plot_starsolo_UMI("quant_without_UMI")
-
-    # remove folder
-    shutil.rmtree("quant_without_UMI")
