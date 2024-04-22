34ee603e7dcf57cfc449ed618665a36e7e6ebea4
Sweta.Talyan <Sweta.Talyan@mpi-bn.mpg.de>
2024-04-22 14:45:21 +0200

    removed extra imports which were not used

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -10,9 +10,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import ipywidgets as widgets
-import functools
 import matplotlib.pyplot as plt
-import glob
 
 from beartype.roar import BeartypeCallHintParamViolation
 
