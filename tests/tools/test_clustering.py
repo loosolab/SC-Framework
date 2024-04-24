a36ada35c5f80457d3387917d721875aa64c1390
Sweta.Talyan <Sweta.Talyan@mpi-bn.mpg.de>
2024-04-24 12:35:12 +0200

    test were added to the tools clustering.py

---
+++ tests/tools/test_clustering.py
@@ -0,0 +1,62 @@
+"""Test clustering functions."""
+
+import pytest
+import numpy as np
+import anndata
+import scanpy as sc
+import pandas as pd
+from scipy import sparse
+import sctoolbox.tools as tl
+
+
+def test_recluster(clust_adata):
+    """Test recluster success."""
+    # join monocytes clusters
+    tl.recluster(adata=clust_adata,
+                 column="louvain",
+                 clusters=["CD14+ Monocytes", "FCGR3A+ Monocytes"],
+                 task="join",
+                 method="leiden",
+                 resolution=1,
+                 key_added="joined_louvain",
+                 plot=True,
+                 embedding="X_umap")
+
+    assert "joined_louvain" in clust_adata.obs.columns
+    assert len(set(clust_adata.obs["louvain"])) - 1 == len(set(clust_adata.obs["joined_louvain"]))
+
+    # split cluster
+    tl.recluster(adata=clust_adata,
+                 column="louvain",
+                 clusters=["CD4 T cells"],
+                 task="split",
+                 method="leiden",
+                 resolution=1,
+                 key_added="split_louvain",
+                 plot=True,
+                 embedding="X_umap")
+
+    assert "split_louvain" in clust_adata.obs.columns
+    assert len(set(clust_adata.obs["louvain"])) < len(set(clust_adata.obs["split_louvain"]))
+
+
+def test_gini():
+    """Test gini function."""
+
+    test_array_equal = np.full(10, 5)
+
+    test_array_unequal = np.zeros(100)
+    test_array_unequal[-1] = 100
+
+    assert tl.gini(test_array_equal) == 0
+    assert tl.gini(test_array_unequal) == 0.99
+
+
+def test_calc_ragi(equal_adata, unequal_adata):
+    """Test calc_ragi function."""
+
+    _, ragi_equal = tl.calc_ragi(equal_adata, "cluster", None)
+    _, ragi_unequal = tl.calc_ragi(unequal_adata, "cluster", None)
+
+    assert ragi_equal == 0
+    assert round(ragi_unequal, 2) == 0.9
