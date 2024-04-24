77c5c798524e15b1e73a0e4b3d7b8022d487222b
Ben Herold <Ben.Herold@mpi-bn.mpg.de>
2024-04-24 13:25:48 +0200

    moved the test functions from test_plotting to test_highly_variable

---
+++ tests/plotting/test_highly_variable.py
@@ -0,0 +1,75 @@
+"""Test plotting/highly_variable functions."""
+
+import pytest
+import sctoolbox.plotting as pl
+import scanpy as sc
+import os
+import numpy as np
+
+# Prevent figures from being shown, we just check that they are created
+plt.switch_backend("Agg")
+
+
+# ------------------------------ FIXTURES --------------------------------- #
+@pytest.fixture(scope="session")  # re-use the fixture for all tests
+def adata():
+    """Load and returns an anndata object."""
+
+    np.random.seed(1)  # set seed for reproducibility
+
+    f = os.path.join(os.path.dirname(__file__), 'data', "adata.h5ad")
+    adata = sc.read_h5ad(f)
+
+    adata.obs["condition"] = np.random.choice(["C1", "C2", "C3"], size=adata.shape[0])
+    adata.obs["clustering"] = np.random.choice(["1", "2", "3", "4"], size=adata.shape[0])
+    adata.obs["cat"] = adata.obs["condition"].astype("category")
+
+    adata.obs["LISI_score_pca"] = np.random.normal(size=adata.shape[0])
+    adata.obs["qc_float"] = np.random.uniform(0, 1, size=adata.shape[0])
+    adata.var["qc_float_var"] = np.random.uniform(0, 1, size=adata.shape[1])
+
+    adata.obs["qcvar1"] = np.random.normal(size=adata.shape[0])
+    adata.obs["qcvar2"] = np.random.normal(size=adata.shape[0])
+
+    sc.pp.normalize_total(adata, target_sum=None)
+    sc.pp.log1p(adata)
+
+    sc.tl.umap(adata, n_components=3)
+    sc.tl.tsne(adata)
+    # sc.tl.pca(adata)
+    sc.tl.rank_genes_groups(adata, groupby='clustering', method='t-test_overestim_var', n_genes=250)
+    # sc.tl.dendrogram(adata, groupby='clustering')
+
+    return adata
+    
+    
+# ------------------------------ TESTS --------------------------------- #
+    
+def test_violin_HVF_distribution(adata):
+    """Test violin_HVF_distribution."""
+    adata_HVF = adata.copy()
+    adata_HVF.var['highly_variable'] = np.random.choice([True, False], size=adata_HVF.shape[1])
+    adata_HVF.var['n_cells_by_counts'] = np.random.normal(size=adata_HVF.shape[1])
+    pl.violin_HVF_distribution(adata_HVF)
+    assert True
+
+
+def test_violin_HVF_distribution_fail(adata):
+    """Test if input is invalid."""
+    with pytest.raises(KeyError):
+        pl.violin_HVF_distribution(adata)
+        
+    
+def test_scatter_HVF_distribution(adata):
+    """Test scatter_HVF_distribution."""
+    adata_HVF = adata.copy()
+    adata_HVF.var['variability_score'] = np.random.normal(size=adata_HVF.shape[1])
+    adata_HVF.var['n_cells'] = np.random.normal(size=adata_HVF.shape[1])
+    pl.scatter_HVF_distribution(adata_HVF)
+    assert True
+
+
+def test_scatter_HVF_distribution_fail(adata):
+    """Test if input is invalid."""
+    with pytest.raises(KeyError):
+        pl.scatter_HVF_distribution(adata)
