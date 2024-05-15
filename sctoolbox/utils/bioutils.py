9f52825907df7c5867f08584ac4b7294c5ee5585
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2024-05-15 11:29:35 +0200

    fix test_bioutils related imports

--- sctoolbox/utils/bioutils.py
+++ sctoolbox/utils/bioutils.py
@@ -506,7 +506,7 @@ def _overlap_two_bedfiles(bed1: str, bed2: str, overlap: str, **kwargs: Any) -> 
     None
     """
     # Ensure that path to bedtools is set
-    pybedtools.helpers.set_bedtools_path(utils._add_path())
+    pybedtools.helpers.set_bedtools_path(utils.checker._add_path())
     # Load the bed files using Pybedtools
     bedfile1 = pybedtools.BedTool(bed1)
     bedfile2 = pybedtools.BedTool(bed2)
