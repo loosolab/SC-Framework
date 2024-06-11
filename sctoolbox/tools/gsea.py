969596a962c814c188ba56ee37414b7ab0834844
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2024-06-11 09:14:46 +0200

    Move library download into enrichr_marker_gene function; keep the option to give custom gene sets

--- sctoolbox/tools/gsea.py
+++ sctoolbox/tools/gsea.py
@@ -3,20 +3,24 @@ import pandas as pd
 import gseapy as gp
 import scanpy as sc
 import tqdm
+import warnings
+
 import sctoolbox.utils.decorator as deco
 from sctoolbox.tools.marker_genes import get_rank_genes_tables
 
 from beartype import beartype
-
+from beartype.typing import Optional
 
 @deco.log_anndata
 @beartype
 def enrichr_marker_genes(adata: sc.AnnData,
                          marker_key: str,
-                         gene_sets: dict[str, list[str]],
                          organism: str,
-                         background: set[str],
-                         pvals_adj_tresh: float = 0.05) -> pd.DataFrame:
+                         pvals_adj_tresh: float = 0.05,
+                         library_name: str = "GO_Biological_Process_2023",
+                         gene_sets: Optional[dict[str, list[str]]] = None,
+                         background: Optional[set[str]] = None
+                         ) -> pd.DataFrame:
     """
     Wrapps enrichr module to use on marker genes per cluster.
 
@@ -26,14 +30,18 @@ def enrichr_marker_genes(adata: sc.AnnData,
         Anndata object.
     marker_key: str
         Key in adata.uns containing rank gene group tables.
-    gene_sets : dict[str, list[str]]
-        Dictionary with pathway names as key and gene set as value.
     organism : str
         Source organism.
-    background : set[str]
-        Set of background genes.
-    pvals_adj_tresh : float
+    pvals_adj_tresh : float, default 0.05
         Threshold for adjusted p-value.
+    library_name : str, default GO_Biological_Process_2023
+        Name of public GO library.
+        Get gene sets from public GO library.
+    gene_sets : Optional[dict[str, list[str]]], default None
+        Dictionary with pathway names as key and gene set as value.
+        If given library name is ignored.
+    background : Optional[set[str]], default None
+        Set of background genes. Will be automatically determined when library_name or gene_sets is given.
 
     Returns
     -------
@@ -44,6 +52,8 @@ def enrichr_marker_genes(adata: sc.AnnData,
     ------
     KeyError:
         If marker_key not in adata.uns
+    ValueError:
+        If result dictinary is empty
     """
     if marker_key in adata.uns:
         marker_tables = get_rank_genes_tables(adata,
@@ -51,9 +61,13 @@ def enrichr_marker_genes(adata: sc.AnnData,
                                               key=marker_key)
     else:
         raise KeyError("Marker key not found! Please check parameter!")
+    
+    if not gene_sets:
+        gene_sets = gp.get_library(name=library_name, organism=organism)
+    if not background:
+        background = set([item for sublist in gene_sets.values() for item in sublist])
 
     path_enr = {}
-
     for ct, table in tqdm.tqdm(marker_tables.items()):
         # subset up or down regulated genes
         degs_sig = table[table["pvals_adj"] < pvals_adj_tresh]
@@ -77,8 +91,13 @@ def enrichr_marker_genes(adata: sc.AnnData,
                 enr_list.append(enr.res2d)
 
         # concat results
-        path_enr[ct] = pd.concat(enr_list)
-        path_enr[ct]["Cluster"] = ct
+        if not all(x is None for x in enr_list):
+            path_enr[ct] = pd.concat(enr_list)
+            path_enr[ct]["Cluster"] = ct
+        else:
+            warnings.warn(f"No valid pathways found for cluster {ct}")
 
     # Return combined table
+    if not path_enr:
+        raise ValueError("No valid pathways found for dataset.")
     return (pd.concat(path_enr.values()))
--- tests/tools/test_gsea.py
+++ tests/tools/test_gsea.py
@@ -10,42 +10,17 @@ import pandas as pd
 
 @pytest.fixture
 def adata():
-    """Load and returns an anndata object."""
-    f = os.path.join(os.path.dirname(__file__), '..', 'data', "adata.h5ad")
-
-    obj = sc.read_h5ad(f)
-
-    # add cluster column
-    def repeat_items(list, count):
-        """
-        Repeat list until size reached.
-
-        https://stackoverflow.com/a/54864336/19870975
-        """
-        return list * (count // len(list)) + list[:(count % len(list))]
-
-    obj.obs["cluster"] = repeat_items([f"cluster {i}" for i in range(10)], len(obj))
-
-    tools.marker_genes.run_rank_genes(obj, "cluster")
-
+    """Return clustered dataset with ranked genes."""
+    obj = sc.datasets.pbmc3k_processed()
+    tools.marker_genes.run_rank_genes(obj, "louvain")
     return obj
 
 
 def test_enrichr_marker_genes(adata):
     """Test enrichr_marker_genes."""
-
-    organism = "human"
-
-    # download a library or read a .gmt file
-    go_mf = gp.get_library(name="GO_Biological_Process_2023", organism=organism)
-    # list of all genes as background
-    flat_list = set([item for sublist in go_mf.values() for item in sublist])
-
     result = tools.gsea.enrichr_marker_genes(adata,
                                              marker_key="rank_genes_cluster_filtered",
-                                             gene_sets=go_mf,
-                                             organism=organism,
-                                             background=flat_list)
+                                             organism="human")
 
     assert isinstance(result, pd.DataFrame)
 
@@ -53,16 +28,7 @@ def test_enrichr_marker_genes(adata):
 def test_fail_enrichr_marker_genes(adata):
     """Test if invalid marker key is caught by enrichr_marker_genes."""
 
-    organism = "human"
-
-    # download a library or read a .gmt file
-    go_mf = gp.get_library(name="GO_Biological_Process_2023", organism=organism)
-    # list of all genes as background
-    flat_list = set([item for sublist in go_mf.values() for item in sublist])
-
     with pytest.raises(KeyError):
         tools.gsea.enrichr_marker_genes(adata,
                                         marker_key="invalid",
-                                        gene_sets=go_mf,
-                                        organism=organism,
-                                        background=flat_list)
+                                        organism="human")
