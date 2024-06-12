f45c1f7dae587d2bf8ea4113054c0300c15cd047
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2024-06-12 15:58:16 +0200

    Added tests for gsea plotting function

---
+++ tests/plotting/test_gsea.py
@@ -0,0 +1,32 @@
+"""Test gsea plotting functions."""
+
+import pytest
+import scanpy as sc
+import pandas as pd
+from sctoolbox.plotting import gsea
+
+
+@pytest.fixture(scope="session")  # re-use the fixture for all tests
+def adata():
+    return sc.datasets.pbmc68k_reduced()
+
+
+@pytest.fixture(scope="session")  # re-use the fixture for all tests
+def term_table():
+    term_table = pd.DataFrame({
+        "Term": "Actin Filament Organization (GO:0007015)",
+        "Genes": ["COBL", "WIPF1;SH3KBP1"]
+        })
+    return term_table
+
+
+def test_term_dotplot(adata, term_table):
+    """Test term_dotplot success."""
+    axes = gsea.term_dotplot(term="Actin Filament Organization (GO:0007015)",
+                             term_table=term_table,
+                             adata=adata,
+                             groupby="louvain")
+
+    assert isinstance(axes, list)
+    ax_type = type(axes[0]).__name__
+    assert ax_type.startswith("Axes")
