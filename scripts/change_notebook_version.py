6b33c979d0ad286f252687d58d0d17a24ba5abb1
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2024-06-13 11:05:14 +0200

    Add notebook version to metadata; Fix update version script

---
+++ scripts/change_notebook_version.py
@@ -0,0 +1,42 @@
+"""
+Script to add/update notebook versions.
+"""
+import nbformat
+import warnings
+import argparse
+from pathlib import Path
+from packaging import version
+
+
+def update_version(notebook, ver):
+    nb = nbformat.read(notebook, as_version=4)
+    if "sc_framework" not in nb["metadata"]:
+        nb["metadata"]["sc_framework"] = dict()
+        nb["metadata"]["sc_framework"]["version"] = ver
+    else:
+        old_ver = nb["metadata"]["sc_framework"]["version"]
+        if version.parse(old_ver) > version.parse(ver):
+            raise ValueError(f"New version ({ver}) is older than notebook version ({old_ver}).")
+        if version.parse(old_ver) == version.parse(ver):
+            warnings.warn(f"New verison is identical to notebook version. ({ver})")
+        nb["metadata"]["sc_framework"]["version"] = ver
+    nbformat.write(nb, notebook)
+
+
+def update_notebooks(repo_path, ver):
+    path = Path(repo_path)
+    for p in path.glob("**/*.ipynb"):
+        update_version(p, ver)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog="change_notebook_version",
+        description="Loops recursivly through given directory and updates the sc_framework version for every notebook."
+    )
+    parser.add_argument("path", type=str, help="Directory containing notebooks.")
+    parser.add_argument("version", type=str, help="New version.")
+    args = parser.parse_args()
+
+    update_notebooks(args.path, args.version)
