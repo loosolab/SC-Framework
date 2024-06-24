130bbc978534736527f9ea3e693c4ad5c0b941c6
rwiegan <rene.wiegandt@mpi-bn.mpg.de>
2024-06-24 15:09:07 +0200

    change_notebook_version.py: Added docstring for functions; minor changes

--- scripts/change_notebook_version.py
+++ scripts/change_notebook_version.py
@@ -8,7 +8,19 @@ from pathlib import Path
 from packaging import version
 
 
-def update_version(notebook, ver, force):
+def update_version(notebook: str, ver: str, force: bool):
+    """
+    Update sc_framework version of notebook.
+    
+    Parameters
+    ----------
+    notebook : str
+        Path of notebook.
+    ver : str
+        New version of notebook.
+    force : bool
+        Ignore warnings and overwrite notebook version.
+    """
     nb = nbformat.read(notebook, as_version=4)
     if "sc_framework" not in nb["metadata"]:
         nb["metadata"]["sc_framework"] = dict()
@@ -17,16 +29,28 @@ def update_version(notebook, ver, force):
         old_ver = nb["metadata"]["sc_framework"]["version"]
         if not force:
             if version.parse(old_ver) > version.parse(ver):
-                warnings.warn(f"New version ({ver}) is older than notebook version ({old_ver}). Skipped {notebook}")
+                warnings.warn(f"New version ({ver}) is older than notebook version ({old_ver}). Skipped {notebook} (set 'force' to overwrite)")
                 return
             if version.parse(old_ver) == version.parse(ver):
-                warnings.warn(f"New version is identical to notebook version. ({ver}). Skipped {notebook}")
+                warnings.warn(f"New version is identical to notebook version. ({ver}). Skipped {notebook} (set 'force' to overwrite)")
                 return
         nb["metadata"]["sc_framework"]["version"] = ver
     nbformat.write(nb, notebook)
 
 
-def update_notebooks(repo_path, ver, force):
+def update_notebooks(repo_path: str, ver: str, force: bool):
+    """
+    Loops recursivly through given directory and updates the sc_framework version for every notebook.
+    
+    Parameters
+    ----------
+    repo_path : str
+        Path of repository/directory containing notebooks.
+    ver : str
+        New version of notebook.
+    force : bool
+        Ignore warnings and overwrite notebook version.
+    """
     path = Path(repo_path)
     for p in path.glob("**/*.ipynb"):
         print(p)
