d0472690fcdbe7aed1731c0ad3de871740b72ca1
Jan Detleffsen <jan.detleffsen@mpi-bn.mpg.de>
2024-07-23 13:31:47 +0200

    added blank line

--- tests/plotting/test_embedding_pl.py
+++ tests/plotting/test_embedding_pl.py
@@ -419,4 +419,3 @@ def test_plot_pca_correlation_fail(adata, kwargs):
 
     with pytest.raises((BeartypeCallHintParamViolation, KeyError, ValueError)):
         pl.plot_pca_correlation(adata, **kwargs)
-
