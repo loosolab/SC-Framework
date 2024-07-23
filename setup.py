5f98a82804f25abf03621d79dddf056a40807c39
Yousef Alayoubi <yousef.alayoubi@mpi-bn.mpg.de>
2024-07-23 15:53:00 +0200

    set scipy version to >=1.14

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
         'kneed',
         'qnorm',
         'plotly',
-        'scipy',
+        'scipy>=1.14',
         'statsmodels',
         'tqdm',
         'pandas>1.5.3',  # https://gitlab.gwdg.de/loosolab/software/sc_framework/-/issues/200
