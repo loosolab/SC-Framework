506f44cf9003bdc533926aa19628f2366be37c7c
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2024-09-30 16:42:22 +0200

    tabs to whitespaces

--- setup.py
+++ setup.py
@@ -66,8 +66,8 @@ setup(
     py_modules=modules,
     python_requires='>=3.9',  # dict type hints as we use it require python 3.9
     install_requires=[
-	'cmake>=3.18',  # fixes ERROR: Failed to build installable wheels for some pyproject.toml based projects (gseapy, louvain)
-	'pysam',
+    'cmake>=3.18',  # fixes ERROR: Failed to build installable wheels for some pyproject.toml based projects (gseapy, louvain)
+    'pysam',
         'matplotlib<3.9.0',
         'matplotlib_venn',
         'scanpy>=1.10.2',  # 'colorbar_loc' not available before 1.9
