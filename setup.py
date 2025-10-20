d44e053b22b82967416df3690264f28a69c0186e
Dlnija Mirza <dlnija.mirza@mpi-bn.mpg.de>
2025-10-20 09:59:49 +0000

    Edit setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ extras_require = {"converter": ['rpy2', 'anndata2ri'],
                   "gsea": ["gseapy"],
                   "deseq2": ["pydeseq2>=0.4.11"],
                   "deseq2": ["pydeseq2>=0.5.2"],  # https://github.com/owkin/PyDESeq2/issues/242
-                  "pilotpy" : ["pilotpy @ git+https://github.com/loosolab/PILOT_scframework.git"]
+                  "pilotpy": ["pilotpy @ git+https://github.com/loosolab/PILOT_scframework.git"]
                   }
 
 # contains all requirements aka a full installation
