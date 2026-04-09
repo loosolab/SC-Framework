7a5f353f38ea44ed4c7199764821b3b9dfc12a5d
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2026-04-09 12:19:30 +0200

    relocate data prep to a fixture

--- tests/plotting/test_highly_variable.py
+++ tests/plotting/test_highly_variable.py
@@ -9,15 +9,32 @@ import matplotlib.pyplot as plt
 plt.switch_backend("Agg")
 
 
+# ------------------------------ FIXTURES --------------------------------- #
+
+
+@pytest.fixture
+def adata_hvf(adata_fun_scope):
+    """AnnData with columns required for HVF distribution plots.
+
+    Returns
+    -------
+    anndata.AnnData
+        AnnData with highly_variable, n_cells_by_counts, variability_score, and n_cells columns.
+    """
+    adata_fun_scope.var['highly_variable'] = np.random.choice([True, False], size=adata_fun_scope.shape[1])
+    adata_fun_scope.var['n_cells_by_counts'] = np.random.normal(size=adata_fun_scope.shape[1])
+    adata_fun_scope.var['variability_score'] = np.random.normal(size=adata_fun_scope.shape[1])
+    adata_fun_scope.var['n_cells'] = np.random.normal(size=adata_fun_scope.shape[1])
+
+    return adata_fun_scope
+
+
 # ------------------------------ TESTS --------------------------------- #
 
 
-def test_violin_HVF_distribution(adata):
+def test_violin_HVF_distribution(adata_hvf):
     """Test violin_HVF_distribution."""
-    adata_HVF = adata.copy()
-    adata_HVF.var['highly_variable'] = np.random.choice([True, False], size=adata_HVF.shape[1])
-    adata_HVF.var['n_cells_by_counts'] = np.random.normal(size=adata_HVF.shape[1])
-    pl.violin_HVF_distribution(adata_HVF)
+    pl.violin_HVF_distribution(adata_hvf)
 
 
 def test_violin_HVF_distribution_fail(adata):
@@ -26,12 +43,9 @@ def test_violin_HVF_distribution_fail(adata):
         pl.violin_HVF_distribution(adata)
 
 
-def test_scatter_HVF_distribution(adata):
+def test_scatter_HVF_distribution(adata_hvf):
     """Test scatter_HVF_distribution."""
-    adata_HVF = adata.copy()
-    adata_HVF.var['variability_score'] = np.random.normal(size=adata_HVF.shape[1])
-    adata_HVF.var['n_cells'] = np.random.normal(size=adata_HVF.shape[1])
-    pl.scatter_HVF_distribution(adata_HVF)
+    pl.scatter_HVF_distribution(adata_hvf)
 
 
 def test_scatter_HVF_distribution_fail(adata):
